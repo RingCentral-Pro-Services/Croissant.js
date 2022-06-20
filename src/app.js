1b80a15fc8dc8d602c9287bc7c4943c913484570
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-20 13:03:20 -0400

    Add dynamic port support

--- src/app.js
+++ src/app.js
@@ -48,6 +48,7 @@ var server = http.createServer(function (req, res) {
 
 });
 
-server.listen(3000); //3 - listen for any incoming requests
+let port = process.env.PORT || 3000
+server.listen(port);
 
-console.log('Node.js web server at port 3000 is running..')
+console.log('Server running on port ' + port)
