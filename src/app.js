650c1aaffe2fb27556dead906c2e8b2ae901a68a
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-20 01:26:42 -0400

    Add support for XML files

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var IVRMenu = require('./IVRMenu.js')
 var ExcelReader = require('./ExcelReader.js')
 var XMLWriter = require('./XMLWriter')
+var XMLReader = require('./XMLReader')
+var AuditWriter = require('./AuditWriter')
 
 var server = http.createServer(function (req, res) {
 
@@ -12,16 +14,33 @@ var server = http.createServer(function (req, res) {
         form.parse(req, function (err, fields, files) {
 
             var filePath = files.filetoupload.filepath;
+            console.log(files.filetoupload.originalFilename)
             let resultingFilename = files.filetoupload.originalFilename.replace(".xlsx", ".xml")
+            
+            if (files.filetoupload.originalFilename.includes('.xlsx')) {
+              // This is an excel file, process accordingly
+              const reader = new ExcelReader(filePath)
+              let menus = reader.getMenus()
+              //console.log(menus[0].prompt)
+              let xmlWriter = new XMLWriter(menus)
 
-            const reader = new ExcelReader(filePath)
-            let menus = reader.getMenus()
-            let xmlWriter = new XMLWriter(menus)
+              res.setHeader('Content-Length', xmlWriter.xmlData.length);
+              res.setHeader('Content-Type', 'text/xml');
+              res.setHeader('Content-Disposition', 'attachment; filename=' + resultingFilename);
+              res.write(xmlWriter.xmlData, 'binary');
+            }
+            else if (files.filetoupload.originalFilename.includes('.xml')) {
+              // This is an XML file, generate audit
+              let xmlReader = new XMLReader(filePath)
+              let menus = xmlReader.getMenus()
+              let auditWriter = new AuditWriter(menus)
 
-            res.setHeader('Content-Length', xmlWriter.xmlData.length);
-            res.setHeader('Content-Type', 'text/xml');
-            res.setHeader('Content-Disposition', 'attachment; filename=' + resultingFilename);
-            res.write(xmlWriter.xmlData, 'binary');
+              res.setHeader('Content-Length', auditWriter.csvData.length);
+              res.setHeader('Content-Type', 'text/xml');
+              res.setHeader('Content-Disposition', 'attachment; filename=' + resultingFilename);
+              res.write(auditWriter.csvData, 'binary');
+            }
+            
             res.end()
      });
       } else {
