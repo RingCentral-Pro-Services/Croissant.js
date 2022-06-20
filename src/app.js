4b564ec9f3db653c324cc1dff93fbf7b70c7e0fb
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-19 20:35:44 -0400

    Serve index.html instead of old form

--- src/app.js
+++ src/app.js
@@ -3,29 +3,30 @@ var formidable = require('formidable');
 var fs = require('fs');
 var IVRMenu = require('./IVRMenu.js')
 var ExcelReader = require('./ExcelReader.js')
+var XMLWriter = require('./XMLWriter')
 
 var server = http.createServer(function (req, res) {
 
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
+
             var filePath = files.filetoupload.filepath;
-            res.write('You uploaded a file: ' + filePath)
+            let resultingFilename = files.filetoupload.originalFilename.replace(".xlsx", ".xml")
 
             const reader = new ExcelReader(filePath)
             let menus = reader.getMenus()
+            let xmlWriter = new XMLWriter(menus)
 
-            console.log(menus[0]["name"])
-
+            res.setHeader('Content-Length', xmlWriter.xmlData.length);
+            res.setHeader('Content-Type', 'text/xml');
+            res.setHeader('Content-Disposition', 'attachment; filename=' + resultingFilename);
+            res.write(xmlWriter.xmlData, 'binary');
             res.end()
      });
       } else {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
-        res.end();
+        fs.createReadStream('index.html').pipe(res)
       }
 
 });
@@ -33,5 +34,3 @@ var server = http.createServer(function (req, res) {
 server.listen(3000); //3 - listen for any incoming requests
 
 console.log('Node.js web server at port 5000 is running..')
-
-const menu = new IVRMenu()
