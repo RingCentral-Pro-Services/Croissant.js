6196037837b278635bd62dff60728ecd77f3c833
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-21 19:32:25 -0400

    Add SpecialKeyPress

---
+++ src/SpecialKeyPress.js
@@ -0,0 +1,11 @@
+class SpecialKeyPress {
+    key = ""
+    actionType = ""
+
+    constructor(key, actionType) {
+        this.key = key
+        this.actionType = actionType
+    }
+}
+
+module.exports = SpecialKeyPress
