3478c155e2f7fe2f67211f661aeb90a131832494
DQ Griffin <dquaviusgriffin@gmail.com>
2022-06-24 22:15:53 -0400

    Update database query

--- src/DatabaseManager.js
+++ src/DatabaseManager.js
@@ -129,7 +129,7 @@ class DatabaseManager {
           
           client.connect();
           
-          client.query(`INSERT INTO menu_data VALUES(${filename.replace(/\s/g, '_')}, ${menuCount}, ${keyPressCount}, ${Date.now().toString().replace(/\s/g, '_')})`, (err, res) => {
+          client.query(`INSERT INTO menu_data VALUES(south, ${menuCount}, ${keyPressCount}, ${Date.now().toString().replace(/\s/g, '_')})`, (err, res) => {
             if (err) {
               console.log('Failed')
               console.log(err)
