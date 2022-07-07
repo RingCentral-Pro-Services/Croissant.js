1a1b7c2f8843cf48f8f8377b8f3a15e8a1d661fa
DQ Griffin <dquaviusgriffin@gmail.com>
2022-07-06 21:08:01 -0400

    Don't subtract one from date

--- src/DatabaseManager.js
+++ src/DatabaseManager.js
@@ -161,7 +161,7 @@ class DatabaseManager {
             let ts = Date.now();
 
             let date_ob = new Date(ts);
-            let date = date_ob.getDate() - 1;
+            let date = date_ob.getDate();
             let month = date_ob.getMonth() + 1;
             let year = date_ob.getFullYear();
 
