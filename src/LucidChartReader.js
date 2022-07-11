1b989f40feedfe9eaf7f72b48cf6678bfbe071e7
DQ Griffin <dquaviusgriffin@gmail.com>
2022-07-11 12:25:20 -0400

    Implement extension number generation

--- src/LucidChartReader.js
+++ src/LucidChartReader.js
@@ -11,6 +11,7 @@ class LucidChartReader {
     done = false
     extensionRegex = /(x)\d+/gi  // Matches x-denoted extension numbers (Ex. x4796)
     extRegex = /(ext)(.?)\s\d+/gi  // Matches "ext." followed by numbers (Ex. ext. 4796)
+    extTBD = /(ext[.]?)([\s]?tbd[\s]?[\d]?)/gi
 
 
     constructor(filepath) {
@@ -44,9 +45,24 @@ class LucidChartReader {
         for (let index = 0; index < this.rowData.length; index++) {
             if (this.rowData[index]["Name"] == "IVR") {
                 let shapeText = this.rowData[index]["Text Area 1"]
+
+                // This code is commented out for now, it's related to the automatic extension number generation
+                // let extensionNumber = ""
+
+                // if (/(ext[.]?)([\s]?tbd)/gi.test(shapeText)) {
+                //     const extensionLength = shapeText.match(this.extTBD).toString().replace(/\D/g,'')
+                //     extensionNumber = this.generateRandomExtension(extensionLength)
+                //     console.log(`TBD Extension Length: ${extensionLength}`)
+                //     console.log(`Generated Extension: ${extensionNumber}`)
+                // }
+                // else {
+                //     extensionNumber = isolator.isolateExtension(shapeText.toString())
+                // }
+
                 let extensionNumber = isolator.isolateExtension(shapeText.toString())
                 let extensionName = shapeText.replace(this.extRegex, "")
                 extensionName = extensionName.replace(this.extensionRegex, "")
+                extensionName = extensionName.replace(this.extTBD, "")
                 extensionName = extensionName.replace("\n", "")
 
                 let id = this.rowData[index]["Id"]
@@ -92,7 +108,12 @@ class LucidChartReader {
                             this.menus[menuIndex].actions.push(action)
                         }
                         else {
+                            // This code is commented out for now, it's related to the automatic extension number generation
                             // Create a forward to extension action and add it to the menu
+                            // if (destinationType == "IVR") {
+                            //     let extensionName = this.getExtensionNameforID(lineDestinationID)
+                            //     extensionNumber = this.getExtensionforMenuName(extensionName.replace(this.extTBD, ""))
+                            // }
                             let action = new IVRKeyPress(key, "ForwardToExtension", extensionNumber)
                             this.menus[menuIndex].actions.push(action)
                         }
@@ -164,7 +185,23 @@ class LucidChartReader {
     }
 
     /**
-     * Check whether or now a menu with thet same extension
+     * Get the extension number 
+     * @param {string} name The name of the menu
+     * @returns The extension number associated with the given menu name or -1
+     * if not found
+     */
+    getExtensionforMenuName(name) {
+        for (let index = 0; index < this.menus.length; index++) {
+            if (this.menus[index].name == name) {
+                return this.menus[index].extensionNumber
+            }
+        }
+        console.log(`No extension for: ${name}|`)
+        return -1
+    }
+
+    /**
+     * Check whether or now a menu with the same extension
      * already exists in the array.
      * @param {string} extensionNumber The extension number of the menu
      * @returns True if a menu with the same extension already exists in the array
@@ -176,6 +213,47 @@ class LucidChartReader {
             }
         }
         return false
+    }
+
+    /**
+     * Generate a random extension number
+     * @param length The length of the generated extension number
+     * @returns The generated extension number 
+     */
+    generateRandomExtension(length) {
+        const floor = this.generateFloor(length)
+        const ceiling = this.generateCeiling(length)
+        return Math.floor(Math.random() * parseInt(ceiling)) + parseInt(floor);
+    }
+
+    /**
+     * Generate the minumum bound of the generated extension number
+     * @param length The length of the generated extension number
+     * @returns The minimum bound of the generated extension number
+     */
+    generateFloor(length) {
+        let floor = "1"
+
+        for (let count = 1; count < length; count++) {
+            // Append a 1 to the floor
+            floor += "1"
+        }
+        return floor
+    }
+
+    /**
+     * Generate the maximum bound of the generated extension number
+     * @param length The length of the generated extension number
+     * @returns The maximum bound of the generated extension number
+     */
+    generateCeiling(length) {
+        let ceiling = "9"
+
+        for (let count = 1; count < length; count++) {
+            // Append a 1 to the floor
+            ceiling += "9"
+        }
+        return ceiling
     }
 }
 
