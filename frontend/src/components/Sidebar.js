489fe4bc21b41c3254bd849d0b5834f6dfb2abf7
DQ Griffin <dquaviusgriffin@gmail.com>
2022-08-29 21:25:46 -0400

    Add logic for nav item selection

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import NavItem from "./NavItem";
 
 const Sidebar = () => {
+    const [selectedItem, setSelectedItem] = useState("Create Menus")
+
+    const handleClick = (text) => {
+        setSelectedItem(text)
+    }
+
     return ( 
         <div className="sidebar">
             <h4>Croissant</h4>
-            <NavItem />
-            <NavItem />
+            <NavItem text="Create Menus" isSelected={selectedItem === "Create Menus"} handleClick={handleClick}/>
+            <NavItem text="Audit Menus" isSelected={selectedItem === "Audit Menus"} handleClick={handleClick} />
         </div>
      );
 }
