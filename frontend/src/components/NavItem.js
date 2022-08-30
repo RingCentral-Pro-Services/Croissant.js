c09f5d6fdd14a577c2da39178b46f95862e9d380
DQ Griffin <dquaviusgriffin@gmail.com>
2022-08-29 20:52:31 -0400

    Add React frontend

---
+++ frontend/src/components/NavItem.js
@@ -0,0 +1,9 @@
+const NavItem = () => {
+    return ( 
+        <div className="nav-item">
+            <p>Create Menus</p>
+        </div>
+     );
+}
+ 
+export default NavItem;
---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import NavItem from "./NavItem";
+
+const Sidebar = () => {
+    return ( 
+        <div className="sidebar">
+            <h4>Croissant</h4>
+            <NavItem />
+            <NavItem />
+        </div>
+     );
+}
+ 
+export default Sidebar;
