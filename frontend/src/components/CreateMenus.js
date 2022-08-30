ffdd5b085155f3f14ce99753d76529731d44e67c
DQ Griffin <dquaviusgriffin@gmail.com>
2022-08-30 19:30:45 -0400

    Save file when data is received

--- frontend/src/components/CreateMenus.js
+++ frontend/src/components/CreateMenus.js
@@ -1,11 +1,24 @@
-import { useFormSubmit } from '../hooks/useFormSubmit'
 import { useState, useEffect } from 'react'
+import ResourcesArea from './ResourcesArea';
 const axios = require('axios').default;
+const FileSaver = require('file-saver');
 
 const CreateMenus = () => {
     const [selectedFile, setSelectedFile] = useState(null);
     const [isPending, setIsPending] = useState(false)
     const [data, setData] = useState(null)
+    const [outputFilename, setOutputFilename] = useState(null)
+
+    const instructionsData = [
+        {text: "Building with the BRD", link: "", id: 1},
+        {text: "Building with Lucidchart", link: "", id: 2}
+    ]
+
+    const rescourcesData = [
+        {text: "Documentation", link: "https://docs.google.com/document/d/1xcF5H6KHnn-UF_vgAq7LEKJBa-g4XKBY3YvH0or-9HQ/edit?usp=sharing", id: 1},
+        {text: "Example BRD", link: "https://docs.google.com/spreadsheets/d/1mEaZVjjmesskNf47Dz0jMgPgZDKZ13PISgqiNyyMNos/edit?usp=sharing", id: 2},
+        {text: "Example Lucidchart", link: "https://lucid.app/lucidchart/51421e0f-912e-47ca-a063-59d43cf436fd/edit?viewport_loc=-1505%2C-1165%2C5370%2C2692%2C0_0&invitationId=inv_50c4ff9e-896f-4ddc-b3df-ccd0251074b5#", id:3}
+    ]
 
     const handleClick = (e) => {
         e.preventDefault()
@@ -20,8 +33,8 @@ const CreateMenus = () => {
         axios
         .post('/fileupload', formData)
         .then((res) => {
-            alert("File Upload success");
-            console.log(res.data)
+            console.log(res.headers["content-disposition"])
+            setOutputFilename(res.headers["content-disposition"].replace("attachment; filename=", ""))
             setData(res.data)
             setIsPending(false)
         })
@@ -29,8 +42,14 @@ const CreateMenus = () => {
     }
 
     useEffect(() => {
-        console.log('Use effect ran')
-    }, [data])
+        if (!data) {
+            return
+        }
+
+        const blob = new Blob([data], {type: "text/plain;charset=utf-8"})
+        FileSaver.saveAs(blob, outputFilename)
+        setData(null)
+    }, [data, outputFilename])
 
     return ( 
         <>
@@ -41,6 +60,8 @@ const CreateMenus = () => {
                 <button type='button' onClick={handleSubmit}>{isPending ? "Waiting for server" : "Submit"}</button>
                 <input id="create-menu-file-select" type="file" onInput={(e) => setSelectedFile(e.target.files[0])} hidden/>
             </form>
+            <ResourcesArea title="Instructions" links={instructionsData}/>
+            <ResourcesArea title="Resources" links={rescourcesData}/>
         </>
      );
 }
