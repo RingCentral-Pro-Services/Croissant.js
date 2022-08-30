5153ae9a90158c4a18c762e46803a3b743701eaf
DQ Griffin <dquaviusgriffin@gmail.com>
2022-08-30 17:50:47 -0400

    Add ResourceArea component

---
+++ frontend/src/components/ResourcesArea.js
@@ -0,0 +1,20 @@
+import linkIcon from '../link.png'
+
+const ResourcesArea = (props) => {
+    const { title, links } = props
+
+    return ( 
+        <div className="rescources">
+            <h3>{ title }</h3>
+            {links.map((link) => (
+                <div className='resource-items' key={link.id}>
+                    <a href={ link.link } target="_blank" rel="noreferrer" className='info-link'>{ link.text }</a>
+                    <img src={ linkIcon } alt="link icon" className='link-icon' />
+                    <br />
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default ResourcesArea;
