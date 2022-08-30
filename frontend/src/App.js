7d4594fd0e838bf8f3ed0ea6bd86190a09f24f1c
DQ Griffin <dquaviusgriffin@gmail.com>
2022-08-30 08:35:01 -0400

    Add route for homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import CreateMenus from './components/CreateMenus';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <div className="content">
+        <Router>
+          <Routes>
+            <Route path='/' element={<CreateMenus />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
