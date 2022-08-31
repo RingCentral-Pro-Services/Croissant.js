95761f3519522a92c661bb5f80d2cfc6b89be578
DQ Griffin <dquaviusgriffin@gmail.com>
2022-08-31 19:26:08 -0400

    Add FilterItem.js

---
+++ frontend/src/components/FilterItem.js
@@ -0,0 +1,12 @@
+const FilterItem = (props) => {
+    const { text, isChecked, handleClick} = props
+
+    return ( 
+        <div className="filter-item" onClick={() => handleClick(text)}>
+            <input type="checkbox" id="select-all-checkbox" className="w3-check" name={text} checked={isChecked} readOnly />
+            <p className="checkbox-label">{ text }</p>
+        </div>
+     );
+}
+ 
+export default FilterItem;
