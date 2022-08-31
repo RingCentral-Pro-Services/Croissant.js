0061cd388cada7f10c97d821292be28ef18cd9bb
DQ Griffin <dquaviusgriffin@gmail.com>
2022-08-31 19:26:17 -0400

    Add PageFilter.js

---
+++ frontend/src/components/PageFilter.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import FilterItem from "./FilterItem";
+
+const PageFilter = (props) => {
+    const { pages, handleFilterClick, handleInput } = props
+    const [filteredPages, setFilteredPages] = useState(null)
+
+    const handleClick = () => {
+        console.log('click')
+        const box = document.getElementById("filter-box")
+        if (box.className.indexOf("w3-show") === -1) {
+            box.className += " w3-show";
+        } else {
+            box.className = box.className.replace(" w3-show", "");
+        }
+    }
+
+    return ( 
+        <div className="dropdown-click">
+            <button type="button" onClick={handleClick}>Filter Site(s)</button>
+            <div className="dropdown-content" id="filter-box">
+                <input className="w3-input w3-padding" type="search" placeholder="Search.." id="myInput"  onInput={handleInput} autoComplete="off" />
+                <FilterItem text="Select All" isChecked={true} handleClick={handleFilterClick}/>
+                {pages.map((page) => (
+                    <FilterItem text={ page.text } isChecked={ page.checked } handleClick={ handleFilterClick } key={page.text} />
+                ))}
+            </div>
+        </div>
+     );
+}
+ 
+export default PageFilter;
