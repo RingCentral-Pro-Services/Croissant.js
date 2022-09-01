2103450b76b2ab8dc93d23dc539c6fae1ccbf8c1
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-01 15:56:32 -0400

    Change port to 3000

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ var path = require("path");
 var LucidChartReader = require('./LucidChartReader')
 const PrettyAuditWriter = require('./PrettyAuditWriter')
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
