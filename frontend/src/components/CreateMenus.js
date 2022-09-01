f5aa1f3926c9c80b3eca27c8717ff243ee4e1e3c
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-01 09:35:47 -0400

    Refactor to move logic to custom hooks

--- frontend/src/components/CreateMenus.js
+++ frontend/src/components/CreateMenus.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import PageFilter from './PageFilter';
 import ResourcesArea from './ResourcesArea';
+import usePageExtractor from '../hooks/usePageExtractor';
+import useFilterServices from '../hooks/useFilterServices';
 const axios = require('axios').default;
 const FileSaver = require('file-saver');
 
@@ -10,7 +12,8 @@ const CreateMenus = () => {
     const [data, setData] = useState(null)
     const [outputFilename, setOutputFilename] = useState(null)
     const [filteredPages, setFilteredPages] = useState(null)
-    const [pages, setPages] = useState([])
+    const {pages, setPages, extract} = usePageExtractor()
+    const {handleFilterClick, handleInput} = useFilterServices(pages, setPages, filteredPages, setFilteredPages)
 
     const instructionsData = [
         {text: "Building with the BRD", link: "", id: 1},
@@ -22,62 +25,6 @@ const CreateMenus = () => {
         {text: "Example BRD", link: "https://docs.google.com/spreadsheets/d/1mEaZVjjmesskNf47Dz0jMgPgZDKZ13PISgqiNyyMNos/edit?usp=sharing", id: 2},
         {text: "Example Lucidchart", link: "https://lucid.app/lucidchart/51421e0f-912e-47ca-a063-59d43cf436fd/edit?viewport_loc=-1505%2C-1165%2C5370%2C2692%2C0_0&invitationId=inv_50c4ff9e-896f-4ddc-b3df-ccd0251074b5#", id:3}
     ]
-
-    useEffect(() => {
-        window.addEventListener('mouseup', (e) => {
-            const filterBox = document.getElementById('filter-box')
-            const filterButton = document.getElementById('filter-button')
-
-            if (e.target != filterButton && !filterBox.contains(e.target)) {
-                filterBox.classList.remove("w3-show")
-            }
-        })
-    }, [])
-
-    const handleFilterClick = (text) => {
-        if (text === "Select All") {
-            let result = [...pages]
-            result.forEach((page) => {
-                page.checked = true
-            })
-            setPages([...result])
-        }
-
-        let result = pages.map((page) => {
-            if (page.text !== text) {
-                return {text: page.text, checked: page.checked}
-            }
-            return {text: page.text, checked: !page.checked}
-        })
-        setPages(result)
-
-        if (filteredPages) {
-            let filtered = filteredPages.map((page) => {
-                if (page.text !== text) {
-                    return {text: page.text, checked: page.checked}
-                }
-                return {text: page.text, checked: !page.checked}
-            })
-            setFilteredPages(filtered)
-        }
-    }
-
-    const handleInput = () => {
-        const input = document.getElementById("myInput").value.toUpperCase()
-
-        if (!input) {
-            setFilteredPages(null)
-            return
-        }
-
-        let result = []
-        pages.forEach((page) => {
-            if (page.text.toUpperCase().includes(input)) {
-                result.push({text: page.text, checked: page.checked})
-            }
-        })
-        setFilteredPages(result)
-    }
 
     const handleClick = (e) => {
         e.preventDefault()
@@ -112,26 +59,10 @@ const CreateMenus = () => {
         setData(null)
     }, [data, outputFilename])
 
-    useEffect(() => {
-        if (!selectedFile) {
-            return
-        }
-        let reader = new FileReader()
-
-        reader.onload = () => {
-            let result = []
-            let rows = reader.result.split("\n")
-            for (let index = 0; index < rows.length; index++) {
-                let elements = rows[index].split(",")
-                if (elements.length >= 12 && elements[1] === "Page") {
-                    console.log(elements[11])
-                    result.push({text: elements[11], checked: true})
-                }
-            }
-            setPages(result)
-        }
-        reader.readAsText(selectedFile)
-    }, [selectedFile])
+    const handleFileSelect = (file) => {
+        setSelectedFile(file)
+        extract(file)
+    }
 
     return ( 
         <>
@@ -141,7 +72,7 @@ const CreateMenus = () => {
                 <p className="inline healthy-margin-right">{selectedFile ? selectedFile.name : "No file selected"}</p>
                 {selectedFile && selectedFile.name.includes('.csv') && <PageFilter pages={filteredPages ? filteredPages : pages} handleFilterClick={handleFilterClick} handleInput={handleInput} />}
                 <button type='button' onClick={handleSubmit}>{isPending ? "Waiting for server" : "Submit"}</button>
-                <input id="create-menu-file-select" type="file" onInput={(e) => setSelectedFile(e.target.files[0])} accept=".xlsx, .xml, .csv" hidden/>
+                <input id="create-menu-file-select" type="file" onInput={(e) => handleFileSelect(e.target.files[0])} accept=".xlsx, .xml, .csv" hidden/>
             </form>
             <ResourcesArea title="Instructions" links={instructionsData}/>
             <ResourcesArea title="Resources" links={rescourcesData}/>
