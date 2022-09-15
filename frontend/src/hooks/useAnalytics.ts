aaa44d110e8c4e82fc83afc6d1d9a7992031b2da
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-14 23:17:53 -0400

    Ignore TypeScript error for now

--- frontend/src/hooks/useAnalytics.ts
+++ frontend/src/hooks/useAnalytics.ts
@@ -1,5 +1,6 @@
 const useAnalytics = () => {
     const fireEvent = (eventName: string) => {
+        // @ts-ignore
         window.gtag('event', eventName, {
             'event_category' : 'dowloads',
             'event_label' : "file"
