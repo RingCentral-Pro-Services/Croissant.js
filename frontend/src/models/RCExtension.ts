50a487db3e061ffec01722884e0eead353220c3b
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-16 19:32:29 -0400

    Add RCExtension.ts

---
+++ frontend/src/models/RCExtension.ts
@@ -0,0 +1,11 @@
+interface RCExtension {
+    uri: string
+    id: number
+    extensionNumber: number
+    name: string
+    type: string
+    status: string
+    hidden: boolean
+}
+
+export default RCExtension
