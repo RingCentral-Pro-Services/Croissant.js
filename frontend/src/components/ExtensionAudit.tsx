3b6c362eb9ea15d5174d0a0210c56684d2d9425e
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-17 16:55:59 -0400

    Dump fetched extension data to screen

--- frontend/src/components/ExtensionAudit.tsx
+++ frontend/src/components/ExtensionAudit.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
+import RCExtension from '../models/RCExtension'
 import useExtensionList from '../rcapi/useExtensionList'
 
 const ExtensionAudit = () => {
-    let [targetUID, setTargetUID] = useState("36479576")
+    let [targetUID, setTargetUID] = useState("~")
     const { extensionsList, isExtensionListPending } = useExtensionList()
 
     const handleClick = () => {
@@ -15,6 +16,11 @@ const ExtensionAudit = () => {
             <input type="text" className="input-field" value={targetUID} onChange={(e) => setTargetUID(e.target.value)}/>
             <button onClick={handleClick}>Go</button>
             <p>{isExtensionListPending ? "Fetching extensions": `${extensionsList.length} extensions fetched`}</p>
+            {extensionsList.map((extension: RCExtension) => (
+                <div key={extension.id}>
+                    <p>{extension.toRow()}</p>
+                </div>
+            ))}
         </>
     )
 }
