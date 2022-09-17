c72214a96e59df08e70983afbab86018e2627f6c
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-17 16:54:36 -0400

    Make RCExtension a class

--- frontend/src/models/RCExtension.ts
+++ frontend/src/models/RCExtension.ts
@@ -1,11 +1,11 @@
-interface RCExtension {
-    uri: string
-    id: number
-    extensionNumber: number
-    name: string
-    type: string
-    status: string
-    hidden: boolean
+import CSVFormattable from "./CSVFormattable"
+
+class RCExtension implements CSVFormattable {
+    constructor(public id: number, public extensionNumber: number, public name: string, public type: string, public status: string, public hidden: boolean , public uri: string) {}
+
+    toRow() {
+        return `${this.name},${this.extensionNumber},${this.type},${this.status},${this.hidden}`
+    }
 }
 
 export default RCExtension
