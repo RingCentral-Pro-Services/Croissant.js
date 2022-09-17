51d9aafea48a87795b940fddea9c94ef7439a128
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-17 00:00:11 -0400

    Show number of extensions fetched

--- frontend/src/components/ExtensionAudit.tsx
+++ frontend/src/components/ExtensionAudit.tsx
@@ -3,7 +3,7 @@ import useExtensionList from '../rcapi/useExtensionList'
 
 const ExtensionAudit = () => {
     let [targetUID, setTargetUID] = useState("36479576")
-    const { isExtensionListPending } = useExtensionList()
+    const { extensionsList, isExtensionListPending } = useExtensionList()
 
     const handleClick = () => {
         console.log('click')
@@ -14,7 +14,7 @@ const ExtensionAudit = () => {
             <h2>Extension Audit</h2>
             <input type="text" className="input-field" value={targetUID} onChange={(e) => setTargetUID(e.target.value)}/>
             <button onClick={handleClick}>Go</button>
-            <p>{isExtensionListPending ? "Fetching extensions": "Get extensions"}</p>
+            <p>{isExtensionListPending ? "Fetching extensions": `${extensionsList.length} extensions fetched`}</p>
         </>
     )
 }
