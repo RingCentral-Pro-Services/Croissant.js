667da8f8e3871ffb0c0348743b9fbe51123799e8
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-17 17:40:00 -0400

    Add site property

--- frontend/src/models/RCExtension.ts
+++ frontend/src/models/RCExtension.ts
@@ -1,10 +1,10 @@
 import CSVFormattable from "./CSVFormattable"
 
 class RCExtension implements CSVFormattable {
-    constructor(public id: number, public extensionNumber: number, public name: string, public type: string, public status: string, public hidden: boolean , public uri: string) {}
+    constructor(public id: number, public extensionNumber: number, public name: string, public site: string, public type: string, public status: string, public hidden: boolean , public uri: string) {}
 
     toRow() {
-        return `${this.name},${this.extensionNumber},${this.type},${this.status},${this.hidden}`
+        return `${this.name},${this.extensionNumber},${this.site},${this.type},${this.status},${this.hidden}`
     }
 }
 
