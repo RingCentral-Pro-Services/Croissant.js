5473a7aecc4433fab120e30ee6199d9349972dc7
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-16 21:29:11 -0400

    Add ExtensionAudit.tsx

---
+++ frontend/src/components/ExtensionAudit.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import useExtensionList from '../rcapi/useExtensionList'
+
+const ExtensionAudit = () => {
+    let [targetUID, setTargetUID] = useState("36479576")
+    const { isExtensionListPending } = useExtensionList()
+
+    const handleClick = () => {
+        console.log('click')
+    }
+
+    return (
+        <>
+            <h2>Extension Audit</h2>
+            <input type="text" className="input-field" value={targetUID} onChange={(e) => setTargetUID(e.target.value)}/>
+            <button onClick={handleClick}>Go</button>
+            <p>{isExtensionListPending ? "Fetching extensions": "Get extensions"}</p>
+        </>
+    )
+}
+
+export default ExtensionAudit
