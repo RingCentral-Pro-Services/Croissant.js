d60313874959c9b18d7216bc7d5752f502f876af
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-17 18:16:23 -0400

    Move token fetching to function

--- frontend/src/rcapi/useGetAccessToken.ts
+++ frontend/src/rcapi/useGetAccessToken.ts
@@ -1,9 +1,13 @@
 // Eventually this hook will submit a fetch request to obtain an access token for a customer account
 // For now, it will just copy the RC user access token
 const useGetAccessToken = () => {
-    let rc_access_token = localStorage.getItem('rc_access_token')
-    if (!rc_access_token) return
-    localStorage.setItem('cs_access_token', rc_access_token)
+    const fetchToken = () => {
+        let rc_access_token = localStorage.getItem('rc_access_token')
+        if (!rc_access_token) return
+        localStorage.setItem('cs_access_token', rc_access_token)
+    }
+
+    return {fetchToken}
 }
 
 export default useGetAccessToken
