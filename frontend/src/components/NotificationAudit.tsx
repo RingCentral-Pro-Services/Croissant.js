8d859df158560c0a06419e2a621b114292add15b
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-18 19:19:43 -0400

    Add NotificationsAudit component

---
+++ frontend/src/components/NotificationAudit.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import useLogin from "../hooks/useLogin"
+import useExtensionList from "../rcapi/useExtensionList"
+
+const NotificationAudit = () => {
+    return (
+        <>
+            <h2>Extension Notifications</h2>
+        </>
+    )
+}
+
+export default NotificationAudit
