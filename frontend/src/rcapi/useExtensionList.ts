e2761a3ffe8da8c70ecc046a091ccdcdc4f8e6da
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-18 12:45:05 -0400

    Account for API rate limiting

--- frontend/src/rcapi/useExtensionList.ts
+++ frontend/src/rcapi/useExtensionList.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import RCExtension from "../models/RCExtension"
+import rateLimit from "../helpers/rateLimit";
 const axios = require('axios').default;
 
 const useExtensionList = () => {
@@ -10,6 +11,7 @@ const useExtensionList = () => {
     let [extensionsList, setExtensionsList] = useState<RCExtension[]>([])
     let [page, setPage] = useState(1)
     let [shouldFetch, setShouldFetch] = useState(false)
+    let [rateLimitInterval, setRateLimitInterval] = useState(0)
 
     const fetchExtensions = () => {
         setExtensionsList([])
@@ -24,35 +26,41 @@ const useExtensionList = () => {
         if (!targetUID) return
         let extensionsURL = `${baseExtensionsURL.replace('~', targetUID)}?page=${page}&perPage=1`
 
-        axios
-        .get(extensionsURL, {
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${accessToken}`
-            }
-        })
-        .then((res: any) => {
-            let resRecords = res.data.records
-            let newRecords = [...extensionsList]
-            for (let index = 0; index < resRecords.length; index++) {
-                let resRecord = resRecords[index]
-                let site = resRecord.site ? resRecord.site.name : null
-                let name = resRecord.name ?? "N/A"
-                let extension = new RCExtension(resRecord.id,resRecord.extensionNumber, name, site, resRecord.type, resRecord.status, resRecord.hidden, resRecord.uri )
-                newRecords.push(extension)
-            }
-            setExtensionsList(newRecords)
+        setTimeout(() => {
+            axios
+            .get(extensionsURL, {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${accessToken}`
+                }
+            })
+            .then((res: any) => {
+                console.log(res)
+                let resRecords = res.data.records
+                let newRecords = [...extensionsList]
+                for (let index = 0; index < resRecords.length; index++) {
+                    let resRecord = resRecords[index]
+                    let site = resRecord.site ? resRecord.site.name : null
+                    let name = resRecord.name ?? "N/A"
+                    let extension = new RCExtension(resRecord.id,resRecord.extensionNumber, name, site, resRecord.type, resRecord.status, resRecord.hidden, resRecord.uri )
+                    newRecords.push(extension)
+                }
+                setExtensionsList(newRecords)
+                setRateLimitInterval(0)
 
-            if (res.data.navigation.nextPage) {
-                setPage(page + 1)
-            }
-            else {
-                setisExtensionListPending(false)
-                setShouldFetch(false)
-                setPage(1)
-            }
-        })
+                if (res.data.navigation.nextPage) {
+                    setRateLimitInterval(rateLimit(res.headers))
+                    setPage(page + 1)
+                }
+                else {
+                    setisExtensionListPending(false)
+                    setShouldFetch(false)
+                    setRateLimitInterval(0)
+                    setPage(1)
+                }
+            })
+        }, rateLimitInterval)
     }, [page, shouldFetch, accessToken, extensionsList])
     
     return {extensionsList, isExtensionListPending, error, fetchExtensions}
