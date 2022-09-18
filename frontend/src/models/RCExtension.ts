54d8c344578d2f618db7086c5f2837e0174391e5
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-17 22:47:10 -0400

    Make CSV data prettier

--- frontend/src/models/RCExtension.ts
+++ frontend/src/models/RCExtension.ts
@@ -1,10 +1,32 @@
 import CSVFormattable from "./CSVFormattable"
 
 class RCExtension implements CSVFormattable {
+
+    prettyType: {[key: string]: string} = {
+        "Department": "Call Queue",
+        "ParkLocation": "Park Location",
+        "IvrMenu": "IVR Menu",
+        "User": "User",
+        "Site": "Site",
+        "DigitalUser": "Digital User",
+        "FaxUser": "Fax User",
+        "FlexibleUser": "Flexible User",
+        "VirtualUser": "Virtual User",
+        "Announcement": "Announcement-Only",
+        "Voicemail": "Message-Only",
+        "SharedLinesGroup": "Shared Line Group",
+        "PagingOnly": "Paging Group",
+        "ApplicationExtension": "Application Extension",
+        "Limited": "Limited Extension",
+        "Bot": "Bot",
+        "ProxyAdmin": "Proxy Admin",
+        "DelegatedLinesGroup": "Delegated Lines Group"
+    }
+
     constructor(public id: number, public extensionNumber: number, public name: string, public site: string, public type: string, public status: string, public hidden: boolean , public uri: string) {}
 
     toRow() {
-        return `${this.name},${this.extensionNumber},${this.site},${this.type},${this.status},${this.hidden}`
+        return `${this.name},${this.extensionNumber ?? 'N/A'},${this.site ?? 'N/A'},${this.prettyType[this.type] ?? this.type},${this.status},${this.hidden}`
     }
 }
 
