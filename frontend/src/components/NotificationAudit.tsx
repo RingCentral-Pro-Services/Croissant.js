f3e4970939da1c7e6d33b1de2f9397558ea0b1ad
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-18 20:41:58 -0400

    Fetch extensions on button click

--- frontend/src/components/NotificationAudit.tsx
+++ frontend/src/components/NotificationAudit.tsx
@@ -1,11 +1,43 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import useLogin from "../hooks/useLogin"
+import { Message, MessageType } from "../models/Message"
 import useExtensionList from "../rcapi/useExtensionList"
+import useGetAccessToken from "../rcapi/useGetAccessToken"
+import useMessageQueue from "../hooks/useMessageQueue"
 
 const NotificationAudit = () => {
+    useLogin()
+    let [targetUID, setTargetUID] = useState("~")
+    const {fetchToken} = useGetAccessToken()
+    let {messages, postMessage} = useMessageQueue()
+    const { extensionsList, isExtensionListPending, fetchExtensions } = useExtensionList(postMessage)
+
+    const handleClick = () => {
+        fetchExtensions()
+    }
+
+    useEffect(() => {
+        localStorage.setItem('target_uid', targetUID)
+        fetchToken()
+    },[targetUID, fetchToken])
+
+    useEffect(() => {
+        if (isExtensionListPending) return
+        postMessage(new Message(`Read ${extensionsList.length} extensions`, MessageType.INFO))
+        
+        // TODO: Add logic to fetch notification settings
+    }, [isExtensionListPending, extensionsList])
+
     return (
         <>
             <h2>Extension Notifications</h2>
+            <input type="text" className="input-field" value={targetUID} onChange={(e) => setTargetUID(e.target.value)}/>
+            <button onClick={handleClick}>Go</button>
+            {messages.map((message: Message) => (
+                <div key={message.body}>
+                    <p className={message.type}>{message.body}</p>
+                </div>
+            ))}
         </>
     )
 }
