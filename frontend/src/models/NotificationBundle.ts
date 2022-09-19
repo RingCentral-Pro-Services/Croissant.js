62488cd833260171d0f7bff7af8125900e52ec18
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-19 16:53:44 -0400

    Convert to interface

--- frontend/src/models/NotificationBundle.ts
+++ frontend/src/models/NotificationBundle.ts
@@ -1,5 +1,10 @@
-class NotificationBundle {
-    constructor(public type: string, notifyByEmail: boolean, notifyBySMS: boolean, advancedEmailAddresses: string[], advancedSMSEmailAddresses: string[], includeAttachment: boolean, markAsRead: boolean) {}
+interface NotificationBundle {
+    notifyByEmail: boolean
+    notifyBySMS: boolean
+    advancedEmailAddresses: string[]
+    advancedSMSEmailAddresses: string[]
+    includeAttachment: boolean
+    markAsRead: boolean
 }
 
 export default NotificationBundle
