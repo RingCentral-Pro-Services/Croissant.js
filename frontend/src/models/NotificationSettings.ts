f4f4fcb10f99e0f29254120fe2994e429de6c163
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-19 16:54:57 -0400

    Use interface for cleaner API

--- frontend/src/models/NotificationSettings.ts
+++ frontend/src/models/NotificationSettings.ts
@@ -1,7 +1,25 @@
+import CSVFormattable from "./CSVFormattable"
 import NotificationBundle from "./NotificationBundle"
+import RCExtension from "./RCExtension"
 
-class NotificationSettings {
-    constructor(public id: number, public emailAddresses: string[], public includeManagers: boolean, public advancedMode: boolean, notificationBundles: NotificationBundle[]) {}
+class NotificationSettings implements CSVFormattable {
+    constructor(public extension: RCExtension, public data: NotificationSettingsPayload) {}
+
+    toRow(): string {
+        return `${this.extension.id},${this.extension.name},${this.extension.extensionNumber},${this.extension.prettyType[this.extension.type]},"${this.data.emailAddresses ?? ""}"`
+    }
+}
+
+export interface NotificationSettingsPayload {
+    uri: string
+    emailAddresses: string[]
+    includeManagers: boolean
+    advancedMode: boolean
+    voicemails: NotificationBundle
+    inboundFaxes: NotificationBundle
+    outboundFaxes: NotificationBundle
+    inboundTexts: NotificationBundle
+    missedCalls: NotificationBundle
 }
 
 export default NotificationSettings
