1d595a7f7e35adb18e06385293a8f4015f88fc12
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-19 11:04:14 -0400

    Remove debug messages

--- frontend/src/helpers/rateLimit.ts
+++ frontend/src/helpers/rateLimit.ts
@@ -4,10 +4,6 @@ const rateLimit = (headers: any) => {
     let timeout = 0
 
     if (requestsRemaining < 1) timeout = interval * 1000
-
-    console.log(`Request Remaining: ${requestsRemaining}`)
-    console.log(`Interval: ${interval}`)
-    console.log(`Timeout: ${timeout}`)
     return timeout
 }
 
