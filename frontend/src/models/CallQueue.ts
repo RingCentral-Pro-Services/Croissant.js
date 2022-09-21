8f335794cec2bdd7e59df2292342c9c5db934816
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-20 22:09:41 -0400

    Implement ExcelFormattable

--- frontend/src/models/CallQueue.ts
+++ frontend/src/models/CallQueue.ts
@@ -1,11 +1,16 @@
 import CSVFormattable from "./CSVFormattable";
+import ExcelFormattable from "./ExcelFormattable";
 import RCExtension from "./RCExtension";
 
-class CallQueue implements CSVFormattable{
+class CallQueue implements CSVFormattable, ExcelFormattable {
     constructor(public extension: RCExtension, public members: string[]) {}
 
     toRow(): string {
         return `${this.extension.name},${this.extension.extensionNumber},${this.extension.site},${this.extension.status},"${this.members}"`
+    }
+
+    toExcelRow(): string[] {
+        return [this.extension.name, `${this.extension.extensionNumber}`, this.extension.site, this.extension.status, `${this.members}`]
     }
 }
 
