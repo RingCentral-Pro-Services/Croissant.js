3add21bfd531fd8323189aa24cdf67dffb4543c0
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-21 17:13:22 -0400

    Add FileSelect.tsx

---
+++ frontend/src/components/FileSelect.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+const FileSelect = (props: {handleSubmit: () => void, setSelectedFile: (file: File) => void, isPending: boolean}) => {
+    const {handleSubmit, setSelectedFile, isPending} = props
+    const [selectedFileName, setSelectedFileName] = useState<string | null>()
+
+    const handleFileOpenClick = () => {
+        document.getElementById('file-select')?.click()
+    }
+
+    const handleFileInput = (e: any) => {
+        setSelectedFile((e.target as HTMLInputElement).files![0])
+        setSelectedFileName((e.target as HTMLInputElement).files![0].name)
+    }
+
+    return (
+        <>
+            <form>
+                <button type='button' className="inline browse-button" onClick={handleFileOpenClick}>Browse...</button>
+                <p className="inline healthy-margin-right">{selectedFileName ? selectedFileName : "No file selected"}</p>
+                <button type='button' onClick={handleSubmit}>{isPending ? "Processing" : "Submit"}</button>
+                <input id="file-select" type="file" onInput={(e) => handleFileInput(e)} accept=".xlsx" hidden/>
+            </form>
+        </>
+    )
+}
+
+export default FileSelect
