e894569d9a74a50726e975ffaf03d4c5bbab61de
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-25 15:05:04 -0400

    Add queue members after queue creation

--- frontend/src/rcapi/useCreateCallQueues.ts
+++ frontend/src/rcapi/useCreateCallQueues.ts
@@ -6,11 +6,13 @@ const axios = require('axios').default;
 const useCreateCallQueues = () => {
     const [queues, setQueues] = useState<CallQueue[]>([])
     let [shouldFetch, setShouldFetch] = useState(false)
+    let [shouldUpdateQueues, setShouldUpdateQueues] = useState(false)
     let [rateLimitInterval, setRateLimitInterval] = useState(0)
     let [currentExtensionIndex, setCurrentExtensionIndex] = useState(0)
     let [isCallQueueCreationPending, setIsPending] = useState(true)
     const accessToken = localStorage.getItem('cs_access_token')
     const url = 'https://platform.devtest.ringcentral.com/restapi/v1.0/account/~/extension'
+    const baseUpdateURL = 'https://platform.devtest.ringcentral.com/restapi/v1.0/account/~/call-queues/groupId/bulk-assign'
 
     const createQueues = (callQueues: CallQueue[]) => {
         console.log(`Creating ${callQueues.length} queues`)
@@ -19,6 +21,7 @@ const useCreateCallQueues = () => {
         setCurrentExtensionIndex(0)
     }
 
+    // Create queues
     useEffect(() => {
         if (!shouldFetch) return
         if (currentExtensionIndex === queues.length) return
@@ -49,15 +52,17 @@ const useCreateCallQueues = () => {
               })
               .then((res: any) => {
                 console.log(res)
+                queues[currentExtensionIndex].extension.id = res.data.id
                 setRateLimitInterval(rateLimit(res.headers))
                 if (currentExtensionIndex !== queues.length - 1) {
                     setCurrentExtensionIndex(currentExtensionIndex + 1)
                 }
                 else {
-                    setIsPending(false)
-                    setShouldFetch(false)
-                    setRateLimitInterval(0)
+                    // setIsPending(false)
                     setCurrentExtensionIndex(0)
+                    setShouldFetch(false)
+                    setShouldUpdateQueues(true)
+                    setRateLimitInterval(0)
                     console.log('Finished creating queues')
                 }
               })
@@ -68,6 +73,52 @@ const useCreateCallQueues = () => {
         }, rateLimitInterval)
 
     }, [shouldFetch, currentExtensionIndex, queues, url])
+
+    useEffect(() => {
+        if (!shouldUpdateQueues) return
+        if (currentExtensionIndex === queues.length) return
+
+        let targetUID = localStorage.getItem('target_uid')
+        if (!targetUID) return
+
+        let queueURL = baseUpdateURL.replace('~', targetUID)
+        queueURL = queueURL.replace('groupId', `${queues[currentExtensionIndex].extension.id}`)
+
+        setTimeout(() => {
+            axios({
+                method: "POST",
+                url: queueURL,
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${accessToken}`
+                },
+                data: {
+                    addedExtensionIds: queues[currentExtensionIndex].members
+                },
+              })
+              .then((res: any) => {
+                console.log(res)
+                setRateLimitInterval(rateLimit(res.headers))
+                if (currentExtensionIndex !== queues.length - 1) {
+                    setCurrentExtensionIndex(currentExtensionIndex + 1)
+                }
+                else {
+                    setIsPending(false)
+                    setShouldFetch(false)
+                    setShouldUpdateQueues(false)
+                    setRateLimitInterval(0)
+                    setCurrentExtensionIndex(0)
+                    console.log('Finished updating queues')
+                }
+              })
+              .catch((error: Error) => {
+                console.log('Something bad happened')
+                console.log(error)
+              })
+        }, rateLimitInterval)
+
+    }, [shouldUpdateQueues])
 
     return {isCallQueueCreationPending, createQueues}
 }
