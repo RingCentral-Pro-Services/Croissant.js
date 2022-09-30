115ba8e8ecdb2ea76a45e0bed7c3c570d19e98c9
DQ Griffin <dquaviusgriffin@gmail.com>
2022-09-30 10:13:58 -0400

    Remove data table div

--- frontend/src/components/DataTable.tsx
+++ frontend/src/components/DataTable.tsx
@@ -6,18 +6,16 @@ const DataTable = (props: {header: string[], data: ExcelFormattable[]}) => {
     const {header, data} = props
 
     return (
-        <div className="data-table">
-            <table>
-                <tr>
-                    {header.map((column) => (
-                        <th>{column}</th>
-                    ))}
-                </tr>
-                {data.map((values) => (
-                    <DataTableRow dataSource={values}/>
+        <table>
+            <tr>
+                {header.map((column) => (
+                    <th>{column}</th>
                 ))}
-            </table>
-        </div>
+            </tr>
+            {data.map((values) => (
+                <DataTableRow dataSource={values}/>
+            ))}
+        </table>
     )
 }
 
