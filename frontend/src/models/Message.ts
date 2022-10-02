2b9a1af33c4f42b7e5d5f9007d9fe8344ee0bcdb
DQ Griffin <dquaviusgriffin@gmail.com>
2022-10-02 17:51:36 -0400

    Use AlertColor instead of message type

--- frontend/src/models/Message.ts
+++ frontend/src/models/Message.ts
@@ -1,9 +1,5 @@
-export class Message {
-    constructor(public body: string, public type: MessageType) {}
-}
+import {AlertColor} from '@mui/material'
 
-export enum MessageType {
-    INFO = 'message-info',
-    ERROR = 'message-error',
-    SUCCESS = 'message-success'
+export class Message {
+    constructor(public body: string, public type: AlertColor) {}
 }
