0c43fb44b9bc080fd567159338e5bf1222f900b0
DQ Griffin <dquaviusgriffin@gmail.com>
2022-10-03 11:44:33 -0400

    Add ExcelSheetSelector.ts

---
+++ frontend/src/components/ExcelSheetSelector.tsx
@@ -0,0 +1,34 @@
+import React, { ChangeEvent, useState } from "react";
+import {Select, MenuItem, SelectChangeEvent, TextField} from '@mui/material'
+
+const ExcelSheetSelector = (props: {sheets: string[], setSelectedSheet: (name: string) => void}) => {
+    const {sheets, setSelectedSheet} = props
+    const [selectedValue, setSelectedValue] = useState<string>('IVRs')
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        console.log('Change detected')
+        console.log(`${event.target.value}`)
+        setSelectedValue(event.target.value)
+        setSelectedSheet(event.target.value)
+    }
+
+    return (
+        <TextField
+            // labelId="demo-simple-select-label"
+            className="vertical-middle healthy-margin-right"
+            select
+            id="demo-simple-select"
+            value={selectedValue}
+            label='Select page'
+            size='small'
+            aria-label='Select page'
+            onChange={handleChange}
+        >
+            {sheets.map((sheet) => (
+                <MenuItem value={sheet}>{sheet}</MenuItem>
+            ))}
+        </TextField>
+    )
+}
+
+export default ExcelSheetSelector
