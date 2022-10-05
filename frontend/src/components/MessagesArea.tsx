e7f8ae25d1b2e42644b62c1aa4cc0785471cbb6c
DQ Griffin <dquaviusgriffin@gmail.com>
2022-10-04 21:19:22 -0400

    Add MessagesArea.tsx

---
+++ frontend/src/components/MessagesArea.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Message } from "../models/Message";
+import { Alert } from '@mui/material'
+
+const MessagesArea = (props: {messages: Message[]}) => {
+    const {messages} = props
+
+    return (
+        <div className="messages-area">
+            {messages.map((message) => (
+                <Alert className="healthy-margin-bottom" severity={message.type}>{message.body}</Alert>
+            ))}
+        </div>
+    )
+}
+
+export default MessagesArea
