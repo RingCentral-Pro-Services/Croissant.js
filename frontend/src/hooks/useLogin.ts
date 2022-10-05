b95a56161e50c70c08fbd2236570f1e02cdbf543
DQ Griffin <dquaviusgriffin@gmail.com>
2022-10-05 10:12:28 -0400

    Redirect to login if token is expired

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,11 +1,24 @@
 const useLogin = () => {
     let token = localStorage.getItem('rc_access_token')
+    let expiration = localStorage.getItem('rc_token_expiry')
 
-    if (!token) {
+    const isTokenExpired = () => {
+        if (!expiration) return true
+
+        let expirationTime = new Date(parseInt(expiration))
+        let currentTime = new Date()
+
+        if (currentTime >= expirationTime) return true
+
+        return false
+    }
+
+    if (!token || isTokenExpired()) {
         let url = `${process.env.REACT_APP_AUTH_BASE}&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_AUTH_REDIRECT}`
         console.log(`URL: ${url}`)
         window.location.replace(url)
     }
+
 }
 
 export default useLogin
