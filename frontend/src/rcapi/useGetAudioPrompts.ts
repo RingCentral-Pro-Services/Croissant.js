bb67d820e5b4abfe08f8e70a42355046edc00f8a
DQ Griffin <dquaviusgriffin@gmail.com>
2022-10-09 15:53:56 -0400

    Add useGetAudioPrompts.ts

---
+++ frontend/src/rcapi/useGetAudioPrompts.ts
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { RestCentral } from "./RestCentral";
+import { AudioPrompt } from "../models/AudioPrompt";
+import { Message } from "../models/Message";
+
+const useGetAudioPrompts = (postMessage: (message: Message) => void, postTimedMessage: (message: Message, duration: number) => void) => {
+    const [isAudioPromptListPending, setIsAudioPromptListPending] = useState(true)
+    const [audioPromptList, setAudioPromptList] = useState<AudioPrompt[]>([])
+    const [page, setPage] = useState(1)
+    const [shouldFetch, setShouldFetch] = useState(false)
+    const [rateLimitInterval, setRateLimitInterval] = useState(0)
+    const baseUrl = 'https://platform.ringcentral.com/restapi/v1.0/account/~/ivr-prompts'
+    const accessToken = localStorage.getItem('cs_access_token')
+
+    const fetchAudioPrompts = () => {
+        setAudioPromptList([])
+        setShouldFetch(true)
+        setIsAudioPromptListPending(true)
+    }
+
+    useEffect(() => {
+        if (!shouldFetch) return
+        if (!accessToken) return
+
+        let pageURL = `${baseUrl}?page=${page}&perPage=100`
+
+        setTimeout(async () => {
+            try {
+                const headers = {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${accessToken}`
+                }
+                let response = await RestCentral.get(pageURL, headers)
+                console.log(response)
+
+                let prompts: AudioPrompt[] = []
+                for (const record of response.data.records) {
+                    const prompt = record
+                    console.log(`Filename: ${record.filename}`)
+                    prompts.push(prompt)
+                }
+                setAudioPromptList(prev => [...prev, ...prompts])
+
+                if (response.data.navigation.nextPage) {
+                    setRateLimitInterval(response.rateLimitInterval)
+                    setPage(page + 1)
+                }
+                else {
+                    setIsAudioPromptListPending(false)
+                    setShouldFetch(false)
+                    setRateLimitInterval(0)
+                    setPage(1)
+                }
+            }
+            catch (e) {
+                console.log('Failed to fetch audio prompts')
+                postMessage(new Message('Failed to fetch audio prompts', 'error'))
+            }
+        }, rateLimitInterval)
+    })
+
+    return {fetchAudioPrompts, audioPromptList, isAudioPromptListPending}
+
+}
+
+export default useGetAudioPrompts
