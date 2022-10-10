4721e6d5f82e92c209279071356e3f873ca45395
DQ Griffin <dquaviusgriffin@gmail.com>
2022-10-09 23:53:11 -0400

    Add mode select

--- frontend/src/components/AuditMenus.tsx
+++ frontend/src/components/AuditMenus.tsx
@@ -1,60 +1,37 @@
 import React from 'react'
 import { useState } from "react";
-import useFileSave from "../hooks/useFileSave";
-import ResourcesArea from './ResourcesArea';
-import useAnalytics from "../hooks/useAnalytics";
 import Header from './Header';
-import {Button} from '@mui/material'
-const axios = require('axios').default;
+import {ToggleButtonGroup, ToggleButton} from '@mui/material'
+import LegactyAuditMenus from './LegacyAuditMenus';
+import DirectAuditMenus from './DirectAuditMenus';
 
 const AuditMenus = () => {
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
-    const [isPending, setIsPending] = useState(false)
-    const {setData, setOutputFilename} = useFileSave()
-    const {fireEvent} = useAnalytics()
+    
+    const [modeSelection, setModeSelection] = useState('direct')
 
-    const instructionsData = [
-        {text: "Auditing Menus", link: "/croissant-audit.pdf", id: 1}
-    ]
-
-    const resourcesData = [
-        {text: "Documentation", link: "https://docs.google.com/document/d/1xcF5H6KHnn-UF_vgAq7LEKJBa-g4XKBY3YvH0or-9HQ/edit?usp=sharing", id: 1}
-    ]
-
-    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault()
-        document.getElementById('audit-menu-file-select')?.click()
-    }
-
-    const handleSubmit = () => {
-        const formData = new FormData();
-        formData.append("filetoupload", selectedFile as File);
-        setIsPending(true)
-
-        axios
-        .post('/audit', formData, {responseType: "blob"})
-        .then((res: any) => {
-            setOutputFilename(res.headers["content-disposition"].replace("attachment; filename=", ""))
-            setData(res.data)
-            setIsPending(false)
-            fireEvent('update-audit')
-        })
-        .catch((err: Error) => alert(err.message));
+    const handleChange = (event: any, newSelection: string) => {
+        if (newSelection === null) return
+        setModeSelection(newSelection)
     }
 
     return ( 
         <>
-            <Header title='Audit IVR Menus' body='Upload an XML file and get an audit-friendly spreadsheet containing all menus in an account'/>
-            <div className='tool-card'>
-            <h2>Audit Menus</h2>
-            <form>
-                <Button variant='outlined' type='button' className="inline browse-button" onClick={handleClick}>Browse...</Button>
-                <p className="inline healthy-margin-right">{selectedFile ? selectedFile.name : "No file selected"}</p>
-                <Button variant='contained' type='button' onClick={handleSubmit}>{isPending ? "Processing" : "Submit"}</Button>
-                <input id="audit-menu-file-select" type="file" onInput={(e) => setSelectedFile((e.target as HTMLInputElement).files![0])} accept=".xml" hidden/>
-            </form>
-            <ResourcesArea title="Instructions" links={instructionsData} />
-            <ResourcesArea title="Resources" links={resourcesData} />
+            <Header title='Audit IVR Menus' body='Generate an audit-friendly list of all IVR menus in an account'/>
+            <div className="tool-card">
+                <h2>Audit Menus</h2>
+                <ToggleButtonGroup
+                    color='primary'
+                    exclusive
+                    value={modeSelection}
+                    size='small'
+                    aria-label='Build Mode'
+                    onChange={handleChange}
+                    className='health-margin-bottom'
+                >
+                    <ToggleButton value='xml'>XML</ToggleButton>
+                    <ToggleButton value='direct'>Direct</ToggleButton>
+                </ToggleButtonGroup>
+                {modeSelection === 'xml' ? <LegactyAuditMenus /> : <DirectAuditMenus />}
             </div>
         </>
      );
