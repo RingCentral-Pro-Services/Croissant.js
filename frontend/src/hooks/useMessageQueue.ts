422baaad01fce5a34572e8e4955359a68307056a
DQ Griffin <dquaviusgriffin@gmail.com>
2022-10-30 18:51:14 -0400

    Add sync error support

--- frontend/src/hooks/useMessageQueue.ts
+++ frontend/src/hooks/useMessageQueue.ts
@@ -1,14 +1,20 @@
 import {useState} from "react"
 import { Message } from "../models/Message"
+import { SyncError } from "../models/SyncError"
 
 const useMessageQueue = () => {
     let [messages, setMessages] = useState<Message[]>([])
+    let [errors, setErrors] = useState<SyncError[]>([])
     
     const postMessage = (message: Message) => {
         setMessages(prev => [...prev, message])
     }
 
-    return {messages, postMessage}
+    const postError = (error: SyncError) => {
+        setErrors(prev => [...prev, error])
+    }
+
+    return {messages, errors, postMessage, postError}
 }
 
 export default useMessageQueue
