4c36712fd61548e0924f1bb5b6fd369a7d26f35d
DQ Griffin <dquaviusgriffin@gmail.com>
2022-11-05 22:58:08 -0400

    Add account uids to locals torage

--- frontend/src/rcapi/useGetAccessToken.ts
+++ frontend/src/rcapi/useGetAccessToken.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { AccountUID } from "../models/AccountUID";
 import useGetCompanyName from "./useGetCompanyName";
 
 const sign = require('jwt-encode');
@@ -6,7 +7,8 @@ const axios = require('axios').default;
 
 const useGetAccessToken = () => {
     const [hasCustomerToken, setHasToken] = useState(false)
-    const {companyName, isCompanyNamePending, getCompanyName} = useGetCompanyName()
+    const [accountID, setAccountID] = useState('')
+    const {companyName, isCompanyNamePending, getCompanyName, reset} = useGetCompanyName()
 
     const fetchToken = (uid: string) => {
         let rc_access_token = localStorage.getItem('rc_access_token')
@@ -33,6 +35,7 @@ const useGetAccessToken = () => {
         })
         .then((res: any) => {
             localStorage.setItem('cs_access_token', res.data['access_token'])
+            setAccountID(uid)
             getCompanyName()
             // setHasToken(true)
         })
@@ -43,8 +46,37 @@ const useGetAccessToken = () => {
 
     useEffect(() => {
         if (isCompanyNamePending) return
+
+        addAccountToLocalStorage()
         setHasToken(true)
+        reset()
     }, [isCompanyNamePending])
+
+    const addAccountToLocalStorage = () => {
+        const accountData: AccountUID = {
+            name: companyName,
+            id: accountID
+        }
+
+        let accounts = localStorage.getItem('accounts')
+        if (!accounts) {
+            let accountList = [accountData]
+            localStorage.setItem('accounts', JSON.stringify(accountList))
+        }
+        else {
+            let accountList = JSON.parse(accounts) as AccountUID[]
+            for (const account of accountList) {
+                if (account.name === companyName) return
+            }
+            if (accountList.length < 5) {
+                accountList.push(accountData)
+            }
+            else {
+                accountList[0] = accountData
+            }
+            localStorage.setItem('accounts', JSON.stringify(accountList))
+        }
+    }
 
     return {fetchToken, hasCustomerToken, companyName}
 }
