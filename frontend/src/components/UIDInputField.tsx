5533c1a1adc8deaedfd9469be86ed9fb7ad41110
DQ Griffin <dquaviusgriffin@gmail.com>
2022-11-05 23:12:04 -0400

    Suggest recently used accounts

--- frontend/src/components/UIDInputField.tsx
+++ frontend/src/components/UIDInputField.tsx
@@ -1,8 +1,36 @@
-import React from "react";
-import {TextField} from '@mui/material'
+import React, { useEffect, useState } from "react";
+import {TextField, Autocomplete} from '@mui/material'
+import { AccountUID } from "../models/AccountUID";
 
 const UIDInputField = (props: {setTargetUID: (value: string) => void, disabled: boolean, disabledText: string}) => {
     const {setTargetUID, disabled, disabledText} = props
+    const [accounts, setAccounts] = useState<AccountUID[]>([])
+
+    useEffect(() => {
+        let accountData = localStorage.getItem('accounts')
+        if (!accountData) return
+
+        setAccounts(JSON.parse(accountData))
+    }, [])
+
+    const handleInput = (e: any, value: string | null) => {
+        if (!value) return
+
+        if (containsLetters(value)) {
+            for (const account of accounts) {
+                if (account.name === value) {
+                    setTargetUID(account.id)
+                }
+            }
+        }
+        else {
+            setTargetUID(value)
+        }
+    }
+
+    const containsLetters = (value: string) => {
+        return /[a-zA-Z]/.test(value);
+    }
 
     if (disabled) {
         return (
@@ -21,33 +49,18 @@ const UIDInputField = (props: {setTargetUID: (value: string) => void, disabled: 
     }
     else {
         return (
-            <TextField 
+            <Autocomplete
                 className="vertical-middle healthy-margin-right"
-                required
-                autoComplete="off"
-                id="outline-required"
-                label="Account UID"
-                defaultValue=""
                 size="small"
-                onChange={(e) => setTargetUID(e.target.value)}
-                disabled={disabled}
-            ></TextField>
+                id="free-solo-demo"
+                sx={{width: 200, display: 'inline-block'}}
+                freeSolo
+                options={accounts.map((account) => account.name)}
+                onChange={handleInput}
+                renderInput={(params) => <TextField {...params} label="Account UID" />}
+            />
         )
     }
-
-    return (
-        <TextField 
-            className="vertical-middle healthy-margin-right"
-            required
-            autoComplete="off"
-            id="outline-required"
-            label="Account UID"
-            defaultValue=""
-            size="small"
-            onChange={(e) => setTargetUID(e.target.value)}
-            disabled={disabled}
-        ></TextField>
-    )
 }
 
 UIDInputField.defaultProps = {
