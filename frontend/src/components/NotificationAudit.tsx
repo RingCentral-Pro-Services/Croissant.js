021dd42f2a0f5abf61cc9e461546781ccfe61b8a
DQ Griffin <dquaviusgriffin@gmail.com>
2022-11-07 12:05:55 -0500

    Implement filtering

--- frontend/src/components/NotificationAudit.tsx
+++ frontend/src/components/NotificationAudit.tsx
@@ -16,6 +16,8 @@ import useSwapNotificationEmails from "../rcapi/useSwapNotificationEmails"
 import useUpdateNotifications from "../rcapi/useUpdateNotifications"
 import useAnalytics from "../hooks/useAnalytics"
 import UIDInputField from "./UIDInputField"
+import AdaptiveFilter from "./AdaptiveFilter"
+import RCExtension from "../models/RCExtension"
 
 const NotificationAudit = () => {
     useLogin()
@@ -31,6 +33,12 @@ const NotificationAudit = () => {
     const [selectedSheet, setSelectedSheet] = useState('')
     const {readFile, excelData, isExcelDataPending} = useReadExcel()
 
+    const prettyExtensionTypes = ['Call Queue', 'Message-Only', 'Shared Line Group', 'User', 'Virtual User']
+    const [selectedExtensionTypes, setSelectedExtensionTypes] = useState<string[]>(prettyExtensionTypes)
+    const [selectedSites, setSelectedSites] = useState<string[]>([])
+    const [sites, setSites] = useState<string[]>([])
+    const [filteredExtensions, setFilteredExtensions] = useState<RCExtension[]>([])
+
     // Progess bar
     const [progressValue, setProgressValue] = useState(0)
     const [maxProgressValue, setMaxProgressValue] = useState(0)
@@ -40,10 +48,11 @@ const NotificationAudit = () => {
 
     const handleClick = () => {
         setIsPending(true)
-        fetchExtensions()
+        fetchNotificationSettings(filteredExtensions)
     }
 
     const handleSyncButtonClick = () => {
+        setIsPending(true)
         setMaxProgressValue(adjustedNotifications.length)
         setProgressValue(0)
         updateNotifications(adjustedNotifications)
@@ -62,6 +71,11 @@ const NotificationAudit = () => {
     },[targetUID])
 
     useEffect(() => {
+        if (!hasCustomerToken) return
+        fetchExtensions()
+    }, [hasCustomerToken])
+
+    useEffect(() => {
         if (isNotificationUpdatePending) return
 
         console.log('Done')
@@ -69,8 +83,21 @@ const NotificationAudit = () => {
 
     useEffect(() => {
         if (isExtensionListPending) return 
-        fetchNotificationSettings(extensionsList)
-        fireEvent('notifications-audit')
+        const extractedSites = extensionsList.filter((extension) => {
+            return extension.prettyType[extension.type] === 'Site'
+        })
+
+        let siteNames = extractedSites.map((site) => {
+            return site.name
+        })
+
+        siteNames = ['Main Site', ...siteNames]
+
+        setSites(siteNames)
+        setSelectedSites(siteNames)
+
+        // fetchNotificationSettings(extensionsList)
+        // fireEvent('notifications-audit')
     }, [isExtensionListPending, extensionsList])
 
     useEffect(() => {
@@ -83,6 +110,18 @@ const NotificationAudit = () => {
         setIsPending(false)
     }, [isNotificationListPending, notifications, writeExcel, isPending])
 
+    useEffect(() => {
+        console.log(selectedExtensionTypes)
+        console.log(selectedSites)
+
+        const filtered = extensionsList.filter((extension) => {
+            return selectedExtensionTypes.includes(extension.prettyType[extension.type]) && selectedSites.includes(extension.site)
+        })
+        console.log(filtered)
+
+        setFilteredExtensions(filtered)
+    }, [selectedExtensionTypes, selectedSites])
+
     return (
         <>
             <Header title="Notifications" body="Audit and update notification emails"/>
@@ -92,7 +131,12 @@ const NotificationAudit = () => {
             <Button disabled={!hasCustomerToken || isPending} variant="contained" onClick={handleClick}>Go</Button>
             {isNotificationListPending ? <></> : <FileSelect isPending={false} enabled={true} setSelectedFile={setSelectedFile} setSelectedSheet={setSelectedSheet} accept='.xlsx' defaultSheet='Notifications' handleSubmit={handleFileSubmit}/>}
             {isEmailSwapPending ? <></> : <Button variant='contained' onClick={handleSyncButtonClick} >Sync</Button>}
-            {false ? <></> : <progress className='healthy-margin-top' id='sync_progress' value={progressValue} max={maxProgressValue} />}
+            <br/>
+            <div className="mega-margin-top">
+                {sites.length > 0 ? <AdaptiveFilter options={prettyExtensionTypes} defaultSelected={prettyExtensionTypes} title='Extension Types' placeholder='Search...' setSelected={setSelectedExtensionTypes} /> : <></>}
+                {sites.length > 0 ? <AdaptiveFilter options={sites} defaultSelected={sites} title='Sites' placeholder='Search...' setSelected={setSelectedSites} /> : <></>}
+            </div>
+            {isPending ? <progress className='healthy-margin-top' id='sync_progress' value={progressValue} max={maxProgressValue} /> : <></>}
             {isEmailSwapPending ? <></> : <FeedbackArea tableHeader={['Mailbox ID', 'Name', 'Ext', 'Type', 'Email Addresses']} tableData={adjustedNotifications} messages={messages} timedMessages={timedMessages} errors={errors} />}
             </div>
         </>
