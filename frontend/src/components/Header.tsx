f8971b6e5669e9ee796ff0144c7caa26ab012b78
DQ Griffin <dquaviusgriffin@gmail.com>
2022-11-11 15:41:00 -0500

    Add support for nested components in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import {IconButton} from '@mui/material'
+import {ExpandMore, ExpandLess} from '@mui/icons-material'
 
-const Header = (props: {title: string, body: string}) => {
-    const {title, body} = props
+const Header = (props: {title: string, body: string, children?: React.ReactNode}) => {
+    const {title, body, children} = props
+    const [isExpanded, setIsExpanded] = useState(false)
 
     return (
         <div className="header">
             <h1>{title}</h1>
             <p>{body}</p>
+            <IconButton sx={{display: children ? 'inline-block' : 'none'}} onClick={() => setIsExpanded(!isExpanded)} >
+                {isExpanded ? <ExpandLess /> : <ExpandMore />}
+            </IconButton>
+            <br/>
+            {isExpanded ? <div className='header-body'>{props.children}</div>: <></>}
         </div>
     )
 }
