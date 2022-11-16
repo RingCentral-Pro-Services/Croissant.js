4e64802df41b4ca5ab6bd9e5613ccfb85680fa90
DQ Griffin <dquaviusgriffin@gmail.com>
2022-11-16 00:00:44 -0500

    Update schemas

--- frontend/src/helpers/schemas.ts
+++ frontend/src/helpers/schemas.ts
@@ -12,7 +12,7 @@ export const ivrSchema = z.object({
     'Menu Ext': z.string({
         required_error: 'Missing Menu Ext',
         invalid_type_error: 'Menu Ext data type invalid'
-    }),
+    }).or(z.number()),
     'Prompt Name/Script': z.string({
         required_error: 'Missing Prompt Name/Script',
         invalid_type_error: 'Prompt Name/Script data type invalid'
@@ -22,61 +22,61 @@ export const ivrSchema = z.object({
     }).optional(),
     'Key 1 Destination': z.string({
         invalid_type_error: 'Key 1 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 2 Action': z.string({
         invalid_type_error: 'Key 2 Action data type invalid',
     }).optional(),
     'Key 2 Destination': z.string({
         invalid_type_error: 'Key 2 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 3 Action': z.string({
         invalid_type_error: 'Key 3 Action data type invalid',
     }).optional(),
     'Key 3 Destination': z.string({
         invalid_type_error: 'Key 3 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 4 Action': z.string({
         invalid_type_error: 'Key 4 Action data type invalid',
     }).optional(),
     'Key 4 Destination': z.string({
         invalid_type_error: 'Key 4 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 5 Action': z.string({
         invalid_type_error: 'Key 5 Action data type invalid',
     }).optional(),
     'Key 5 Destination': z.string({
         invalid_type_error: 'Key 5 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 6 Action': z.string({
         invalid_type_error: 'Key 6 Action data type invalid',
     }).optional(),
     'Key 6 Destination': z.string({
         invalid_type_error: 'Key 6 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 7 Action': z.string({
         invalid_type_error: 'Key 7 Action data type invalid',
     }).optional(),
     'Key 7 Destination': z.string({
         invalid_type_error: 'Key 7 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 8 Action': z.string({
         invalid_type_error: 'Key 8 Action data type invalid',
     }).optional(),
     'Key 8 Destination': z.string({
         invalid_type_error: 'Key 8 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 9 Action': z.string({
         invalid_type_error: 'Key 9 Action data type invalid',
     }).optional(),
     'Key 9 Destination': z.string({
         invalid_type_error: 'Key 9 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
     'Key 0 Action': z.string({
         invalid_type_error: 'Key 0 Action data type invalid',
     }).optional(),
     'Key 0 Destination': z.string({
         invalid_type_error: 'Key 0 Destination data type invalid',
-    }).optional(),
+    }).or(z.number()).optional(),
 })
 
 export const callQueueSchema = z.object({
@@ -87,7 +87,7 @@ export const callQueueSchema = z.object({
     'Extension': z.string({
         required_error: 'Missing extension number',
         invalid_type_error: 'Extension number data type invalid'
-    }),
+    }).or(z.number()),
     'Email': z.string({
         required_error: 'Missing email',
         invalid_type_error: 'Email data type invalid'
