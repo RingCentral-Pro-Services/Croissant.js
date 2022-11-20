1654eea90750922dfa638ca0b8a220d36b6f4c9d
DQ Griffin <dquaviusgriffin@gmail.com>
2022-11-19 21:01:32 -0500

    Replace values with type annotation

--- frontend/src/models/CallHandlingRules.ts
+++ frontend/src/models/CallHandlingRules.ts
@@ -1,11 +1,11 @@
 export interface CallHandlingRules {
-    transferMode: 'Rotating' | 'Simultaneous' | 'FixedOrder'
-    noAnswerAction: 'WaitPrimaryMembers' | 'WaitPrimaryAndOverflowMembers' | 'Voicemail' | 'TransferToExtension' | 'UnconditionalForwarding'
+    transferMode: string
+    noAnswerAction: string
     fixedOrderAgents?: string[]
-    holdAudioInterruptionMode: 'Never' | 'WhenMusicEnds' | 'Periodically'
-    holdAudioInterruptionPeriod?: 15 | 20 | 25 | 30 | 40 | 50 | 60
-    holdTimeExpirationAction: 'Voicemail' | 'TransferToExtension' | 'UnconditionalForwarding'
-    agentTimeout?: 10 | 15 | 20 | 30 | 45 | 60 | 120 | 300
-    holdTime: 0 | 10 | 15 | 20 | 25 | 30 | 60 | 120 | 180 | 240 | 300 | 600 | 900
+    holdAudioInterruptionMode: string
+    holdAudioInterruptionPeriod?: number
+    holdTimeExpirationAction: string
+    agentTimeout?: number
+    holdTime: number
     wrapUpTime?: number 
 }
