b79d6f627a7ea4f4ed13e32e8f1cf47c3115bbd3
DQ Griffin <dquaviusgriffin@gmail.com>
2022-11-20 00:36:52 -0500

    Add hold music presets

--- frontend/src/models/Greetings.ts
+++ frontend/src/models/Greetings.ts
@@ -134,4 +134,112 @@ export namespace Presets {
     }
 }
 
+// I don't know why, but hold music has its own presets that must be used instead of the
+// presets above
+export namespace HoldPresets {
+    export const defaultVoicemail: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/131329',
+        id: '131329',
+        name: 'Default'
+    }
+    export const ringtones: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/131843',
+        id: '131843',
+        name: 'Ring Tones'
+    }
+    export const acousticMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197382',
+        id: '197382',
+        name: 'Acoustic'
+    }
+    export const beautifulMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197383',
+        id: '197383',
+        name: 'Beautiful'
+    }
+    export const classicalMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197384',
+        id: '197384',
+        name: 'Classical'
+    }
+    export const corporateMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197385',
+        id: '197385',
+        name: 'Corporate'
+    }
+    export const countryMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197386',
+        id: '197386',
+        name: 'Country'
+    }
+    export const holidayMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197387',
+        id: '197387',
+        name: 'Holiday'
+    }
+    export const latinMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197388',
+        id: '197388',
+        name: 'Latin'
+    }
+    export const modernJazzMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197389',
+        id: '197389',
+        name: 'Modern Jazz'
+    }
+    export const natureMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197390',
+        id: '197390',
+        name: 'Nature'
+    }
+    export const reggaeMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197391',
+        id: '197391',
+        name: 'Reggae'
+    }
+    export const worldMusic: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197392',
+        id: '197392',
+        name: 'World'
+    }
+    export const musicDisabled: GreetingPreset = {
+        uri: 'https://platform.ringcentral.com/restapi/v1.0/dictionary/greeting/197376',
+        id: '197376',
+        name: 'None'
+    }
+
+    export const presetForSelection = (selection: string) => {
+        switch (selection) {
+            case 'Ring Tones':
+                return ringtones
+            case 'Music (Acoustic)':
+                return acousticMusic
+            case 'Music (Beautiful)':
+                return beautifulMusic
+            case 'Music (Classical)':
+                return classicalMusic
+            case 'Music (Corporate)':
+                return corporateMusic
+            case 'Music (Country)':
+                return countryMusic
+            case 'Music (Holiday)':
+                return holidayMusic
+            case 'Music (Latin America)':
+                return latinMusic
+            case 'Music (Modern Jazz)':
+                return modernJazzMusic
+            case 'Music (Nature)':
+                return natureMusic
+            case 'Music (Reggae)':
+                return reggaeMusic
+            case 'Music (World)':
+                return worldMusic
+            case 'None':
+                return musicDisabled
+            default:
+                return acousticMusic
+        }
+    }
+}
+
 let f = Presets.acousticMusic
