ffeeabe027c2fd470dccac8c53cea2d6144baa5c
DQ Griffin <dquaviusgriffin@gmail.com>
2022-11-19 19:25:20 -0500

    Add Keys.ts

---
+++ frontend/src/helpers/Keys.ts
@@ -0,0 +1,19 @@
+const CallQueueKeys = {
+    'queueName': 'Queue Name',
+    'extension': 'Extension',
+    'email': 'Email',
+    'site': 'Site',
+    'members': 'Members (Ext)',
+    'hours': 'Hours',
+    'greeting': 'Greeting',
+    'audioWhileConnecting': 'Audio While Connecting',
+    'holdMusic': 'Hold Music',
+    'interruptAudio': 'Interrupt Audio',
+    'interruptPrompt': 'Interrupt Prompt',
+    'ringType': 'Ring Type',
+    'userRingTime': 'User Ring Time',
+    'totalRingTime': 'Total Ring Time',
+    'wrapUpTime': 'Wrap Up Time',
+    'voicemailGreeting': 'Voicemail Greeting',
+    'afterHoursBehavior': 'After Hours Behavior'
+}
