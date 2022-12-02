d84038a04f5747f7e51356182182d9015e512f26
DQ Griffin <dquaviusgriffin@gmail.com>
2022-12-02 18:22:27 -0500

    Add RegionalFormat.ts

---
+++ frontend/src/models/RegionalFormat.ts
@@ -0,0 +1,12 @@
+export interface RegionalFormat {
+    uri: string
+    id: string
+    name: string
+    isoCode: string
+    localeCode: string
+    ui: boolean
+    greeting: boolean
+    formattingLocale: boolean
+    timeFormat: string
+    dateFormat: string
+}
