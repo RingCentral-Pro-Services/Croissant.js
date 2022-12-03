35996cf1a174c5bce3b34f8351036efb2d5238f5
DQ Griffin <dquaviusgriffin@gmail.com>
2022-12-02 23:55:30 -0500

    Add RegionalSettings.ts

---
+++ frontend/src/models/RegionalSettings.ts
@@ -0,0 +1,18 @@
+export interface RegionalSettings {
+    homeCountry: {
+        id: string
+    }
+    timezone: {
+        id: string
+    }
+    language: {
+        id: string
+    }
+    greetingLanguage: {
+        id: string
+    }
+    formattingLocale: {
+        id: string
+    }
+    timeFormat: string
+}
