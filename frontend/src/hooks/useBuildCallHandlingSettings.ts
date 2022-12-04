002e455a592b503bab4e9c443e5a1e9df5845f5b
DQ Griffin <dquaviusgriffin@gmail.com>
2022-12-03 23:40:31 -0500

    Add support for wait time routing

--- frontend/src/hooks/useBuildCallHandlingSettings.ts
+++ frontend/src/hooks/useBuildCallHandlingSettings.ts
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
+import ExtensionIsolator from "../helpers/ExtensionIsolator"
 import { CallHandlingRules } from "../models/CallHandlingRules"
+import RCExtension from "../models/RCExtension"
+import {TransferPayload, UnconditionalForwardingPayload} from '../models/TransferPayload'
 
-const useBuildCallHandlingSettings = () => {
+const useBuildCallHandlingSettings = (extensions: RCExtension[]) => {
     const [ringType, setRingType] = useState('')
     const [userRingTime, setUserRingTime] = useState('')
     const [wrapUpTime, setWrapUpTime] = useState('')
@@ -64,7 +67,7 @@ const useBuildCallHandlingSettings = () => {
     const actionMap: Map<string, string> = new Map([
         ['Send new callers to voicemail', 'Voicemail'],
         ['Advise callers of heavy call volume and disconnect', 'Announcement'],
-        ['Send new callers to extensions', 'TransferToExtension'],
+        ['Send new callers to extension', 'TransferToExtension'],
         ['Forward new callers to external number', 'UnconditionalForwarding'],
         ['Voicemail', 'Voicemail'],
         ['Extension', 'TransferToExtension'],
@@ -177,6 +180,125 @@ const useBuildCallHandlingSettings = () => {
         setCallHandling(newCallHandling)
     }, [maxWaitTimeAction])
 
+    // Call Handling: Max wait time destination
+    useEffect(() => {
+        if (maxWaitTimeDestination === '' || !maxWaitTimeDestination) {
+            let newCallHandling = {...callHandling}
+            if (maxWaitTimeAction === 'Extension' && newCallHandling.transfer) {
+                let newTransfers = newCallHandling.transfer.filter((transfer) => transfer.action != 'HoldTimeExpiration')
+                newCallHandling.transfer = newTransfers
+            }
+            else if (maxWaitTimeAction === 'External number' && newCallHandling.unconditionalForwarding) {
+                let newForwarding = newCallHandling.unconditionalForwarding.filter((forward) => forward.action != 'HoldTimeExpiration')
+                newCallHandling.unconditionalForwarding = newForwarding
+            }
+            setCallHandling(newCallHandling)
+            return
+        }
+        
+        if (maxWaitTimeAction === 'Extension') {
+            // Isolate extension number, add transfer
+            const isolator = new ExtensionIsolator()
+            const extension = isolator.isolateExtension(maxWaitTimeDestination)
+            if (!extension) return
+            const id = idForExtension(extension)
+            addTransfer('HoldTimeExpiration', `${id}`)
+        }
+        else if (maxWaitTimeAction === 'External number') {
+            // Isolate phone number, add unconditional forward
+            const isolator = new ExtensionIsolator()
+            const phoneNumber = isolator.isolatePhoneNumber(maxWaitTimeDestination)
+            if (!phoneNumber) return
+            const forward: UnconditionalForwardingPayload = {
+                phoneNumber: phoneNumber,
+                action: "HoldTimeExpiration"
+            }
+            addUnconditionalForwarding('HoldTimeExpiration', phoneNumber)
+        }
+    }, [maxWaitTimeDestination])
+
+    // Call Handling: Max callers destination
+    useEffect(() => {
+        if (queueFullAction === '' || !queueFullAction) {
+            let newCallHandling = {...callHandling}
+            if (queueFullAction === 'Send new callers to extension' && newCallHandling.transfer) {
+                let newTransfers = newCallHandling.transfer.filter((transfer) => transfer.action != 'MaxCallers')
+                newCallHandling.transfer = newTransfers
+            }
+            else if (queueFullAction === 'Forward new callers to external number' && newCallHandling.unconditionalForwarding) {
+                let newForwarding = newCallHandling.unconditionalForwarding.filter((forward) => forward.action != 'MaxCallers')
+                newCallHandling.unconditionalForwarding = newForwarding
+            }
+            setCallHandling(newCallHandling)
+            return
+        }
+        
+        if (queueFullAction === 'Send new callers to extension') {
+            // Isolate extension number, add transfer
+            const isolator = new ExtensionIsolator()
+            const extension = isolator.isolateExtension(queueFullDestination)
+            if (!extension) return
+            const id = idForExtension(extension)
+            addTransfer('MaxCallers', `${id}`)
+        }
+        else if (queueFullAction === 'Forward new callers to external number') {
+            // Isolate phone number, add unconditional forward
+            const isolator = new ExtensionIsolator()
+            const phoneNumber = isolator.isolatePhoneNumber(queueFullDestination)
+            if (!phoneNumber) return
+            const forward: UnconditionalForwardingPayload = {
+                phoneNumber: phoneNumber,
+                action: "MaxCallers"
+            }
+            addUnconditionalForwarding('MaxCallers', phoneNumber)
+        }
+    }, [queueFullDestination])
+
+    const addTransfer = (action: string, id: string) => {
+        let newCallHandling = {...callHandling}
+        let transfers = newCallHandling.transfer
+        for (let index = 0; index < transfers!.length; index++) {
+            if (transfers![index].action === action) {
+                transfers![index].extension.id = id
+                setCallHandling(newCallHandling)
+                return
+            }
+        }
+        const transfer: TransferPayload = {
+            extension: {
+                id: id
+            },
+            action: action
+        }
+        newCallHandling.transfer?.push(transfer)
+        setCallHandling(newCallHandling)
+    }
+
+    const addUnconditionalForwarding = (action: string, phoneNumber: string) => {
+        let newCallHandling = {...callHandling}
+        let forwarding = newCallHandling.unconditionalForwarding
+        for (let index = 0; index < forwarding!.length; index++) {
+            if (forwarding![index].action === action) {
+                forwarding![index].phoneNumber = phoneNumber
+                setCallHandling(newCallHandling)
+                return
+            }
+        }
+        const forward: UnconditionalForwardingPayload = {
+            phoneNumber: phoneNumber,
+            action: action
+        }
+        newCallHandling.unconditionalForwarding?.push(forward)
+        setCallHandling(newCallHandling)
+    }
+
+    const idForExtension = (extensionNumber: string) => {
+        for (const extension of extensions) {
+            if (`${extension.extensionNumber}` == extensionNumber) return extension.id
+        }
+        return -1
+    }
+
     useEffect(() => {
         const newPayload = {
             ...(callHandling.agentTimeout != -1 && {agentTimeout: callHandling.agentTimeout}),
@@ -186,13 +308,11 @@ const useBuildCallHandlingSettings = () => {
             ...(maxCallersPayload != -1 && {maxCallers: maxCallersPayload}),
             ...(callHandling.maxCallersAction != '' && {maxCallersAction: callHandling.maxCallersAction}),
             ...(callHandling.holdTime != -1 && {holdTime: callHandling.holdTime}),
-            ...(callHandling.holdTimeExpirationAction != '' && {holdTimeExpirationAction: callHandling.holdTimeExpirationAction})
+            ...(callHandling.holdTimeExpirationAction != '' && {holdTimeExpirationAction: callHandling.holdTimeExpirationAction}),
+            ...(callHandling.unconditionalForwarding?.length != 0 && {unconditionalForwarding: callHandling.unconditionalForwarding}),
+            ...(callHandling.transfer!.length > 0 && {transfer: callHandling.transfer})
         }
-        console.log('Call Handling')
-        console.log(callHandling)
 
-        console.log('Payload')
-        console.log(newPayload)
         setPayload(newPayload)
     }, [callHandling])
 
