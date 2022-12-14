b91fcd468ab5822856b7d2dee367d4924e6732f2
DQ Griffin <dquaviusgriffin@gmail.com>
2022-12-13 19:36:49 -0500

    Implement route-based code-splitting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useEffect } from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import AuditMenus from './components/pages/IVR/AuditMenus';
-import CallQueues from './components/pages/Call Queues/CallQueues';
+// import AuditMenus from './components/pages/IVR/AuditMenus';
+// import CallQueues from './components/pages/Call Queues/CallQueues';
 import CreateMenus from './components/pages/IVR/CreateMenus';
 import ExtensionAudit from './components/pages/Account Dump/ExtensionAudit';
 import ExtensionDeleter from './components/pages/Delete Extensions/ExtensionDeleter';
@@ -17,6 +17,9 @@ import Deskphones from './components/pages/Deskphones/Deskphones';
 import CustomRules from './components/pages/Custom Rules/CustomRules';
 import ManipulateCustomRules from './components/pages/Custom Rules/ManipulateCustomRules';
 
+const AuditMenus = React.lazy(() => import('./components/pages/IVR/AuditMenus'));
+const CallQueues = React.lazy(() => import('./components/pages/Call Queues/CallQueues'));
+
 function App() {
 
   useEffect(() => {
@@ -28,24 +31,26 @@ function App() {
       <div className="App">
         <Sidebar />
         <div className="content">
-          <Routes>
-            <Route path='/token' element={<Token />} />
-            <Route path='/' element={<CreateMenus />} />
-            <Route path='/auditmenus' element={<AuditMenus />} />
-            <Route path='/accountdump' element={<ExtensionAudit />} />
-            <Route path='/notificationsaudit' element={<NotificationAudit />} />
-            <Route path='/callqueues' element={<CallQueues />} />
-            <Route path='/auditcallqueues' element={<CallQueues />} />
-            <Route path='/createcallqueues' element={<CreateCallQueues />} />
-            <Route path='/callqueuetemplates' element={<CallQueueTemplates />} />
-            <Route path='/deleteextensions' element={<ExtensionDeleter />} />
-            {/* <Route path='/generateprompts' element={<PromptGeneration />} /> */}
-            <Route path='/editsites' element={<EditSites />} />
-            <Route path='/editextensions' element={<ExtensionEditor />} />
-            <Route path='/deskphones' element={<Deskphones />} />
-            <Route path='/copycustomrules' element={<CustomRules />} />
-            <Route path='/customruleedit' element={<ManipulateCustomRules />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path='/token' element={<Token />} />
+              <Route path='/' element={<CreateMenus />} />
+              <Route path='/auditmenus' element={<AuditMenus />} />
+              <Route path='/accountdump' element={<ExtensionAudit />} />
+              <Route path='/notificationsaudit' element={<NotificationAudit />} />
+              <Route path='/callqueues' element={<CallQueues />} />
+              <Route path='/auditcallqueues' element={<CallQueues />} />
+              <Route path='/createcallqueues' element={<CreateCallQueues />} />
+              <Route path='/callqueuetemplates' element={<CallQueueTemplates />} />
+              <Route path='/deleteextensions' element={<ExtensionDeleter />} />
+              {/* <Route path='/generateprompts' element={<PromptGeneration />} /> */}
+              <Route path='/editsites' element={<EditSites />} />
+              <Route path='/editextensions' element={<ExtensionEditor />} />
+              <Route path='/deskphones' element={<Deskphones />} />
+              <Route path='/copycustomrules' element={<CustomRules />} />
+              <Route path='/customruleedit' element={<ManipulateCustomRules />} />
+            </Routes>
+          </Suspense>
         </div>
       </div>
     </Router>
