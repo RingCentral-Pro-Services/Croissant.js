35ccab803b562f6305a711a6b24accabc1bdefbc
DQ Griffin <dquaviusgriffin@gmail.com>
2022-12-21 21:00:31 -0500

    Add support for record group name

--- frontend/src/models/ExtensionContact.ts
+++ frontend/src/models/ExtensionContact.ts
@@ -2,6 +2,10 @@ interface ExtensionContact {
     firstName: string
     lastName: string
     email: string
+    pronouncedName?: {
+        type: string
+        text: string
+    }
 }
 
 export default ExtensionContact
