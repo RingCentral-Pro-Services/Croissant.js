d44f2c399dbbc100aca2a983ce8752b5096b76f0
DQ Griffin <dquaviusgriffin@gmail.com>
2023-01-01 22:16:18 -0500

    Auto-split names when necessasry

--- frontend/src/components/pages/Extension Upload/hooks/useExcelToExtensions.ts
+++ frontend/src/components/pages/Extension Upload/hooks/useExcelToExtensions.ts
@@ -11,17 +11,28 @@ const useExcelToExtensions = () => {
         let extensions: Extension[] = []
         for (let index = 0; index < excelData.length; index++) {
             const currentItem = excelData[index]
+            let firstName = currentItem['First Name']
+            let lastName = currentItem['Last Name']
+            const type = currentItem['User Type']
+
+            if ((type === 'User' || type === 'VirtualUser') && (!lastName || lastName === '')) {
+                const result = evenlySplit(firstName)
+                firstName = result[0]
+                lastName = result[1]
+            }
+
+
             let data: ExtensionData = {
                 id: randomNumberBetween(111111111, 999999999),
                 contact: {
-                    firstName: currentItem['First Name'],
-                    lastName: currentItem['Last Name'],
+                    firstName: firstName,
+                    lastName: lastName,
                     email: currentItem['Email'],
                     department: currentItem['Dept']
                 },
                 extensionNumber: currentItem['Extension'],
                 name: `${currentItem['First Name']} ${currentItem['Last Name'] ? ` ${currentItem['Last Name']}` : ''}`,
-                type: type(currentItem['User Type']),
+                type: translateType(currentItem['User Type']),
                 site: {
                     id: idForSite(currentItem['Site Name'], extensionList),
                     name: currentItem['Site Name']
@@ -38,7 +49,33 @@ const useExcelToExtensions = () => {
         setIsExtensionConverPending(false)
     }
 
-    const type = (rawType: string) => {
+    /*
+    * Sometimes customers don't follow instructions and put the entire name in the first name column.
+    * This function splits the name into first and last name if it contains a space.
+    * If it doesn't contain a space, it returns the raw input as the first name and 'Extension' as the last name.
+    */
+    const evenlySplit = (raw: string) => {
+        if (raw.includes(' ')) {
+            var middle = Math.floor(raw.length / 2)
+            var before = raw.lastIndexOf(' ', middle)
+            var after = raw.indexOf(' ', middle + 1)
+
+            if (middle - before < after - middle) {
+                middle = before
+            } else {
+                middle = after
+            }
+
+            var s1 = raw.substring(0, middle)
+            var s2 = raw.substring(middle + 1)
+            return [s1, s2]
+        }
+        else {
+            return [raw, 'Extension']
+        }
+    }
+
+    const translateType = (rawType: string) => {
         switch (rawType) {
             case 'User':
                 return 'User'
