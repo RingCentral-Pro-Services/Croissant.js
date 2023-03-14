13023b9fd62cca65eb99d00048b6ce5a9897b286
DQ Griffin <dquaviusgriffin@gmail.com>
2023-03-14 19:22:14 -0400

    Remove unused import

--- frontend/src/components/pages/Custom Rules/CustomRulesBuilder.tsx
+++ frontend/src/components/pages/Custom Rules/CustomRulesBuilder.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import useLogin from "../../../hooks/useLogin";
 import useMessageQueue from "../../../hooks/useMessageQueue";
