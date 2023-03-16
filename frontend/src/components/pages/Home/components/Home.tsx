6dabcc56668e74bcb44ebab5a4fec53a3c696e9f
DQ Griffin <dquaviusgriffin@gmail.com>
2023-03-16 17:25:05 -0400

    Updated home stuff

--- frontend/src/components/pages/Home/components/Home.tsx
+++ frontend/src/components/pages/Home/components/Home.tsx
@@ -1,25 +1,28 @@
 import { Autocomplete, Box, TextField, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import ToolCard from "./ToolCard";
 
 const Home = () => {
-    const highlightedTools: Tool[] = [
-        {name: 'Create IVRs', route: '/', description: 'Create and update IVR menus using the BRD or a Lucidchart diagram', tags: ['IVR', 'IVRs', 'BRD', 'Lucidchart', 'Menu', 'Menus', 'Create', 'Update', 'Edit', 'Build', 'Key', 'Key Press']},
-        {name: 'Create Call Queues', route: '/createcallqueues', description: 'Create and update call queues using the BRD', tags: ['']},
-        {name: 'Create Custom Rules', route: '/customrules', description: 'Create and update custom rules using the BRD or a Lucidchart diagram', tags: ['']},
-        {name: 'Upload Extensions', route: '/extensionupload', description: 'Upload users, limited extensions, message-only extensions, and more', tags: ['']},
-        {name: 'Upload Extensions', route: '/extensionupload', description: 'Upload users, limited extensions, message-only extensions, and more', tags: ['']},
-        {name: 'Upload Extensions', route: '/extensionupload', description: 'Upload users, limited extensions, message-only extensions, and more', tags: ['']},
-        {name: 'Upload Extensions', route: '/extensionupload', description: 'Upload users, limited extensions, message-only extensions, and more', tags: ['']},
-    ]
+  const [searchTerm, setSearchTerm] = useState('')
+  
+  const highlightedTools: Tool[] = [
+      {name: 'Create IVRs', route: '/', description: 'Create and update IVR menus using the BRD or a Lucidchart diagram', tags: ['IVR', 'IVRs', 'BRD', 'Lucidchart', 'Menu', 'Menus', 'Create', 'Update', 'Edit', 'Build', 'Key', 'Key Press']},
+      {name: 'Create Call Queues', route: '/createcallqueues', description: 'Create and update call queues using the BRD', tags: ['Queue', 'Queues', 'Call Queue', 'BRD', 'Create', 'Update', 'Edit', 'Build']},
+      {name: 'Create Custom Rules', route: '/customrules', description: 'Create and update custom rules using the BRD or a Lucidchart diagram', tags: ['']},
+      {name: 'Upload Extensions', route: '/extensionupload', description: 'Upload users, limited extensions, message-only extensions, and more', tags: ['']},
+      {name: 'Upload Extensions', route: '/extensionupload', description: 'Upload users, limited extensions, message-only extensions, and more', tags: ['']},
+      {name: 'Upload Extensions', route: '/extensionupload', description: 'Upload users, limited extensions, message-only extensions, and more', tags: ['']},
+      {name: 'Upload Extensions', route: '/extensionupload', description: 'Upload users, limited extensions, message-only extensions, and more', tags: ['']},
+  ]
 
     return (
         <div className='home-container'>
             <Autocomplete
+            className="home-container-input"
         freeSolo
         id="free-solo-2-demo"
         disableClearable
-        options={highlightedTools.map((option) => option.name)}
+        options={highlightedTools.map((option) => option)}
         renderInput={(params) => (
           <TextField
             {...params}
@@ -32,7 +35,9 @@ const Home = () => {
         )}
         renderOption={(props, option) => (
             <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
-              {<p className="bruh">{option}</p>} <br/> {<Typography variant='caption'>{highlightedTools.find(tool => tool.name === option)?.description}</Typography>}
+              {option.name}
+              <br />
+              {option.description}
             </Box>
           )}
       />
