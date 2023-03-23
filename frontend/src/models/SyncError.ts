a059b5c49aeda292b6a23847412a8f1212955c9a
DQ Griffin <dquaviusgriffin@gmail.com>
2023-03-23 11:53:20 -0400

    Tweak some stuff

--- frontend/src/models/SyncError.ts
+++ frontend/src/models/SyncError.ts
@@ -4,6 +4,6 @@ export class SyncError implements ExcelFormattable {
     constructor(public extensionName: string, public extensionNumber: number, public error: string[], public platformResponse?: string, public object?: any) {}
 
     toExcelRow(): string[] {
-        return [this.extensionName, `${this.extensionNumber}`, ...this.error, this.platformResponse ?? '', JSON.stringify(this.object) ?? '']
+        return [this.extensionName, `${this.extensionNumber}`, ...this.error, this.platformResponse ?? '', this.object ? JSON.stringify(this.object) : '']
     }
 }
