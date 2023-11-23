9d6cf5cf2e06bb3c69f696c263487de680cc2ffa
DQ Griffin <dquaviusgriffin@gmail.com>
2023-11-23 13:53:54 -0500

    Fix typo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -44,7 +44,7 @@ import UploadDevices from './components/pages/Device Upload/UploadDevices';
 import ManageCustomFields from './components/pages/Custom Fields/ManageCustomFields';
 import { AssignCustomFields } from './components/pages/Custom Fields/AssignCustomFields';
 import { CustomFields } from './components/pages/Custom Fields/CustomFields';
-import { ConvertCallQueues } from './components/pages/Convertion/Call Queue - Ring Group/ConvertCallQueues';
+import { ConvertCallQueues } from './components/pages/Conversion/Call Queue - Ring Group/ConvertCallQueues';
 
 const AuditMenus = React.lazy(() => import('./components/pages/IVR/AuditMenus'));
 const CallQueues = React.lazy(() => import('./components/pages/Call Queues/CallQueues'));
