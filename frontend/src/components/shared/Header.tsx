c1eb2d5be4d4e5ec4b6b047e187f3fcd6d3bce60
DQ Griffin <dquaviusgriffin@gmail.com>
2024-04-14 21:15:56 -0400

    Fix header margin

--- frontend/src/components/shared/Header.tsx
+++ frontend/src/components/shared/Header.tsx
@@ -18,11 +18,11 @@ const Header = (props: HeaderProps) => {
 
     return (
         <div className="header">
-            <div>
+            <div className="header-container">
                 <h2>{title}</h2>
                 {documentationURL ? <FeedIcon /> : <></>}
                 {documentationURL ? <a href={documentationURL} target="_blank">Documentation</a>: <></>}
-                <Button variant='subtle' onClick={onHelpButtonClick}>Need help?</Button>
+                <Button className="healthy-margin-left" variant='subtle' onClick={onHelpButtonClick}>Need help?</Button>
             </div>
             {/* <p>{body}</p> */}
         </div>
